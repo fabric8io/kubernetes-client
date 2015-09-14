ac5b1710ce7317f0f974131caf9564a27c40938b
Jimmi Dyson <jimmidyson@gmail.com>
2015-09-14 18:33:45 +0100

    Fixes fabric8io/fabric8#4724, fixes fabric8io/fabric8#4717

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/HasMetadataOperation.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/HasMetadataOperation.java
@@ -29,6 +29,7 @@ import java.lang.reflect.InvocationTargetException;
 public class HasMetadataOperation<K extends KubernetesClient, T extends HasMetadata, L extends KubernetesResourceList, D extends Doneable<T>, R extends ClientResource<T, D>>
   extends BaseOperation<K, T, L, D, R> {
 
+  private boolean reaping;
 
   protected HasMetadataOperation(K client, String resourceT, String namespace, String name, Boolean cascading, T item) {
     super(client, resourceT, namespace, name, cascading, item);
@@ -46,9 +47,10 @@ public class HasMetadataOperation<K extends KubernetesClient, T extends HasMetad
       @Override
       public void visit(T resource) {
         try {
-          if (isCascading()) {
+          if (isCascading() && !reaping) {
             Reaper reaper = ReaperFactory.getReaper(oper);
             if (reaper != null) {
+              reaping = true;
               reaper.reap();
             }
           }
