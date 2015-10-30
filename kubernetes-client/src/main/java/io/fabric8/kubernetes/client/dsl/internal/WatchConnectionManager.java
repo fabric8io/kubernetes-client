59718c4938fcf39bfc22e20a5120c07d35474ba9
Ioannis Canellos <iocanel@gmail.com>
2015-10-30 16:42:26 +0200

    Further decrease exposure of the http client.

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/WatchConnectionManager.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/WatchConnectionManager.java
@@ -59,13 +59,14 @@ public class WatchConnectionManager<T, L extends KubernetesResourceList> impleme
   private WebSocketCall webSocketCall;
   private OkHttpClient clonedClient;
 
-  public WatchConnectionManager(final BaseOperation<T, L, ?, ?> baseOperation, final String version, final Watcher<T> watcher, final int reconnectInterval, final int reconnectLimit) throws InterruptedException, ExecutionException, MalformedURLException {
+  public WatchConnectionManager(final OkHttpClient clonedClient, final BaseOperation<T, L, ?, ?> baseOperation, final String version, final Watcher<T> watcher, final int reconnectInterval, final int reconnectLimit) throws InterruptedException, ExecutionException, MalformedURLException {
     if (version == null) {
       KubernetesResourceList currentList = baseOperation.list();
       this.resourceVersion = new AtomicReference<>(currentList.getMetadata().getResourceVersion());
     } else {
       this.resourceVersion = new AtomicReference<>(version);
     }
+    this.clonedClient = clonedClient;
     this.baseOperation = baseOperation;
     this.watcher = watcher;
     this.reconnectInterval = reconnectInterval;
@@ -98,8 +99,6 @@ public class WatchConnectionManager<T, L extends KubernetesResourceList> impleme
       .url(httpUrlBuilder.build())
       .addHeader("Origin", requestUrl.getProtocol() + "://" + requestUrl.getHost() + ":" + requestUrl.getPort())
       .build();
-
-    clonedClient = baseOperation.getClient().clone(); // Clone to make a customized OkHttp for this request.
     clonedClient.setReadTimeout(0, TimeUnit.MILLISECONDS);
 
     webSocketCall = WebSocketCall.create(clonedClient, request);
