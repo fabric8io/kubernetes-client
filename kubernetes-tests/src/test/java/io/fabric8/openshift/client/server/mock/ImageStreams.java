115279929a4debbc954c2d893c824702e273d598
James Strachan <james.strachan@gmail.com>
2017-06-23 08:51:41 +0100

    fix(images): fix image api group
    ImageStream / ImageStreamTag are in the image.openshift.io not build.openshift.io API groups!

---
+++ kubernetes-tests/src/test/java/io/fabric8/openshift/client/server/mock/ImageStreams.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fabric8.openshift.client.server.mock;
+
+import io.fabric8.kubernetes.client.KubernetesClientException;
+import io.fabric8.openshift.api.model.ImageStream;
+import io.fabric8.openshift.api.model.ImageStreamList;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
+
+import java.util.List;
+
+public class ImageStreams {
+  public static void main(String[] args) {
+    try {
+      OpenShiftClient client = new DefaultOpenShiftClient();
+      ImageStreamList list = client.imageStreams().list();
+      if (list == null) {
+        System.out.println("ERROR no list returned!");
+        return;
+      }
+      List<ImageStream> items = list.getItems();
+      for (ImageStream item : items) {
+        System.out.println("ImageStream " + item.getMetadata().getName() + " has version: " + item.getApiVersion());
+      }
+      System.out.println("Found " + items.size() + " ImageStream(s)");
+    } catch (KubernetesClientException e) {
+      System.out.println("Failed: " + e);
+      e.printStackTrace();
+    }
+  }
+
+}
