e8d4047ea674d769472eb49751c9ea63575d82c5
Piyush Garg <piyushgarg001@gmail.com>
2018-01-23 12:24:17 +0530

    Refactored Example Code and Added Description in CHANGELOG.md

--- kubernetes-examples/src/main/java/io/fabric8/openshift/examples/SecurityContextConstraintExample.java
+++ kubernetes-examples/src/main/java/io/fabric8/openshift/examples/SecurityContextConstraintExample.java
@@ -15,22 +15,27 @@
  */
 package io.fabric8.openshift.examples;
 
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.openshift.api.model.SecurityContextConstraints;
 import io.fabric8.openshift.api.model.SecurityContextConstraintsBuilder;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SecurityContextConstraintExample {
 
-    //You need to be login as admin on Openshift for this Example
+    private static final Logger logger = LoggerFactory.getLogger(SecurityContextConstraintExample.class);
+
+    //You need to be login as admin on OpenShift for this Example
     //command for that is
     //oc login -u system:admin
 
-    public static void main(String args[]) {
+    public static void main(String[] args) throws InterruptedException {
 
-        OpenShiftClient client = new DefaultOpenShiftClient();
+        try (OpenShiftClient client = new DefaultOpenShiftClient()) {
 
-        SecurityContextConstraints scc = new SecurityContextConstraintsBuilder()
+          SecurityContextConstraints scc = new SecurityContextConstraintsBuilder()
             .withNewMetadata().withName("scc").endMetadata()
             .withAllowPrivilegedContainer(true)
             .withNewRunAsUser()
@@ -49,8 +54,20 @@ public class SecurityContextConstraintExample {
             .addToGroups("admin-group")
             .build();
 
-        client.securityContextConstraints().create(scc);
+          log("Created SecurityContextConstraints", client.securityContextConstraints().create(scc));
 
-        client.close();
+          client.close();
+
+        } catch (KubernetesClientException e) {
+          logger.error(e.getMessage(), e);
+        }
     }
+
+    private static void log(String action, Object obj) {
+    logger.info("{}: {}", action, obj);
+  }
+
+    private static void log(String action) {
+    logger.info(action);
+  }
 }
