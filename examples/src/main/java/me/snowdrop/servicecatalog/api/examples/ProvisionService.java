641cc6f6cf62ab9c840bb5eeffa9fb864f96e7ae
Ioannis Canellos <iocanel@gmail.com>
2018-06-20 16:25:21 +0300

    chore: Update copyright.

--- examples/src/main/java/me/snowdrop/servicecatalog/api/examples/ProvisionService.java
+++ examples/src/main/java/me/snowdrop/servicecatalog/api/examples/ProvisionService.java
@@ -1,6 +1,5 @@
-/**
- * Copyright (C) 2018 Red Hat, inc.
- *
+/*
+ * Copyright (C) 2018 Red Hat inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- **/
+ */
 package me.snowdrop.servicecatalog.api.examples;
 
 import me.snowdrop.servicecatalog.api.model.ServiceInstance;
