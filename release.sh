961b2fcc1bf658aefa5869802a68bdf098ef89df
Ioannis Canellos <iocanel@gmail.com>
2018-07-06 16:16:37 +0300

    feat: Add release script.

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#
+# Script requires `grab`. To install it:
+# curl -s https://raw.githubusercontent.com/shellib/grab/master/install.sh | bash
+#
+
+source $(grab github.com/shellib/maven as maven)
+
+maven::release
