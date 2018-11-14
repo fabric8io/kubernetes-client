845787219de57a4055446c82282714cffae1cc12
Freeman Fang <freeman.fang@gmail.com>
2018-11-14 10:56:56 +0800

    [ENTESB-9785]Support Running on Java 11/kubernetes-client

--- platforms/karaf/itests/src/test/java/io/fabric8/kubernetes/karaf/itests/FeatureInstallationTest.java
+++ platforms/karaf/itests/src/test/java/io/fabric8/kubernetes/karaf/itests/FeatureInstallationTest.java
@@ -24,8 +24,10 @@ import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.container.internal.JavaVersionUtil;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+import org.ops4j.pax.exam.options.extra.VMOption;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
 
@@ -60,7 +62,46 @@ public class FeatureInstallationTest extends TestBase {
     @Configuration
     public Option[] config() {
         MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf-minimal").versionAsInProject().type("tar.gz");
-        return new Option[]{
+        if (JavaVersionUtil.getMajorVersion() >= 9) {
+            return new Option[]{
+                                karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
+                                configureSecurity().disableKarafMBeanServerBuilder(),
+                                keepRuntimeFolder(),
+                                editConfigurationFilePut("etc/system.properties", "features.xml", System.getProperty("features.xml")),
+                                editConfigurationFileExtend(
+                                    "etc/org.ops4j.pax.url.mvn.cfg",
+                                    "org.ops4j.pax.url.mvn.repositories",
+                                    "file:"+System.getProperty("features.repo")+"@id=local@snapshots@releases"),
+                                logLevel(LogLevelOption.LogLevel.INFO),new VMOption("--add-reads=java.xml=java.logging"),
+                                new VMOption("--add-exports=java.base/"
+                                    + "org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
+                                new VMOption("--patch-module"),
+                                new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-" 
+                                + System.getProperty("karaf.version", "4.2.2-SNAPSHOT") + ".jar"),
+                                new VMOption("--patch-module"),
+                                new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-" 
+                                + System.getProperty("karaf.version", "4.2.2-SNAPSHOT") + ".jar"),
+                                new VMOption("--add-opens"),
+                                new VMOption("java.base/java.security=ALL-UNNAMED"),
+                                new VMOption("--add-opens"),
+                                new VMOption("java.base/java.net=ALL-UNNAMED"),
+                                new VMOption("--add-opens"),
+                                new VMOption("java.base/java.lang=ALL-UNNAMED"),
+                                new VMOption("--add-opens"),
+                                new VMOption("java.base/java.util=ALL-UNNAMED"),
+                                new VMOption("--add-opens"),
+                                new VMOption("java.naming/javax.naming.spi=ALL-UNNAMED"),
+                                new VMOption("--add-opens"),
+                                new VMOption("java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED"),
+                                new VMOption("--add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED"),
+                                new VMOption("--add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED"),
+                                new VMOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
+                                new VMOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
+                                new VMOption("-classpath"),
+                                new VMOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*")
+                            };
+        } else {
+            return new Option[]{
                 karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
                 configureSecurity().disableKarafMBeanServerBuilder(),
                 keepRuntimeFolder(),
@@ -70,6 +111,7 @@ public class FeatureInstallationTest extends TestBase {
                     "org.ops4j.pax.url.mvn.repositories",
                     "file:"+System.getProperty("features.repo")+"@id=local@snapshots@releases"),
                 logLevel(LogLevelOption.LogLevel.INFO),
-        };
+            };
+        }
     }
 }
