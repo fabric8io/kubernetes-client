fead985fe0ac05693085c4adc395017cd33fc524
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-04-24 21:28:14 +0530

    losing validationg for name being null

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/base/BaseOperation.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/base/BaseOperation.java
@@ -251,6 +251,14 @@ public class BaseOperation<T, L extends KubernetesResourceList, D extends Doneab
 
   @Override
   public R withName(String name) {
+    HasMetadata h = (HasMetadata) item;
+    String value = h.getMetadata() != null ? h.getMetadata().getName() : null;
+    if(value != null || !value.isEmpty()){
+      return (R) newInstance(context.withName(name));
+    }
+    if (name == null || name.isEmpty()) {
+      throw new IllegalArgumentException("Name must be provided.");
+    }
     return (R) newInstance(context.withName(name));
   }
 
