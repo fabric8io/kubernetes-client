1e91cd467eb3f805139cbb4d8b1ae896b74a7838
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-05-01 17:36:01 +0530

    Test added in PodTest

--- kubernetes-tests/src/test/java/io/fabric8/kubernetes/client/mock/PodTest.java
+++ kubernetes-tests/src/test/java/io/fabric8/kubernetes/client/mock/PodTest.java
@@ -447,6 +447,14 @@ public class PodTest {
     KubernetesClient client = server.getClient();
     client.pods().inNamespace("ns1").withName("pod2").dir("/etc/hosts").copy(Files.temporaryFolder().toPath());
   }
+
+  @Test(expected = KubernetesClientException.class)
+  public void testCreateOrReplace() {
+    KubernetesClient client = server.getClient();
+    Pod pod = client.pods().inNamespace("ns1").load(getClass().getResourceAsStream("/test-pod.yml")).get();
+    client.pods().inNamespace("ns1").createOrReplace(pod);
+  }
+
   @Test
   public void testListFromServer() {
     PodBuilder podBuilder = new PodBuilder()
