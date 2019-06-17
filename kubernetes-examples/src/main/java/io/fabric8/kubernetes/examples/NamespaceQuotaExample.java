567fa1807b658d70b59a7bbd173dd28b51599ddc
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-06-17 13:29:08 +0530

    requested changes made

--- kubernetes-examples/src/main/java/io/fabric8/kubernetes/examples/NamespaceQuotaExample.java
+++ kubernetes-examples/src/main/java/io/fabric8/kubernetes/examples/NamespaceQuotaExample.java
@@ -41,27 +41,31 @@ public class NamespaceQuotaExample {
 
     Config config = new ConfigBuilder().withMasterUrl(master).build();
     try (final KubernetesClient client = new DefaultKubernetesClient(config)) {
+      String namespace = "namespacetest";
+      if (args.length > 2) {
+        namespace = args[2];
+      }
       try  {
         // Creating namespace
-        Namespace ns = new NamespaceBuilder().withNewMetadata().withName("namespacetest").addToLabels("hello", "world").endMetadata().build();
+        Namespace ns = new NamespaceBuilder().withNewMetadata().withName(namespace).addToLabels("hello", "world").endMetadata().build();
         log("Created namespace", client.namespaces().create(ns));
 
         // Get namespace by name
-        log("Get namespace by name", client.namespaces().withName("namespacetest").get());
+        log("Get namespace by name", client.namespaces().withName(namespace).get());
         // Get namespace by label
         log("Get namespace by label", client.namespaces().withLabel("hello", "world").list());
 
         ResourceQuota quota = new ResourceQuotaBuilder().withNewMetadata().withName("quota-example").endMetadata().withNewSpec().addToHard("pods", new Quantity("5")).endSpec().build();
-        log("Create resource quota", client.resourceQuotas().inNamespace("namespacetest").create(quota));
+        log("Create resource quota", client.resourceQuotas().inNamespace(namespace).create(quota));
 
         try {
-          log("Get jobs in namespace", client.batch().jobs().inNamespace("namespacetest").list());
+          log("Get jobs in namespace", client.batch().jobs().inNamespace(namespace).list());
         } catch (APIGroupNotAvailableException e) {
           log("Skipping jobs example - extensions API group not available");
         }
       } finally {
         // Delete namespace
-        log("Deleted namespace", client.namespaces().withName("namespacetest").delete());
+        log("Deleted namespace", client.namespaces().withName(namespace).delete());
       }
     } catch (Exception e) {
       e.printStackTrace();
