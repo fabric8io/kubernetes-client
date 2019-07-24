fce60d13bd3f4e98353686a75e24c4cbe819352e
Ioannis Canellos <iocanel@gmail.com>
2019-07-24 20:37:55 +0300

    fix: license headers.

--- extensions/tekton/tests/src/test/java/io/fabric8/tekton/test/AdaptTest.java
+++ extensions/tekton/tests/src/test/java/io/fabric8/tekton/test/AdaptTest.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2018 Red Hat inc.
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.fabric8.tekton.test;
 
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
