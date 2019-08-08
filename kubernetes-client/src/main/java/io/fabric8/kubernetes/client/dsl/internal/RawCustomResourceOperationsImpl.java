e4ee3f308c55f21d23f0b1020ac8387c365f8578
George Gastaldi <gegastaldi@gmail.com>
2019-08-08 11:52:10 -0300

    chore: RawCustomResourceOperationsImpl#makeCall now closes the created Response object

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/RawCustomResourceOperationsImpl.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/dsl/internal/RawCustomResourceOperationsImpl.java
@@ -16,6 +16,7 @@
 package io.fabric8.kubernetes.client.dsl.internal;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.fabric8.kubernetes.api.model.Status;
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
@@ -31,7 +32,6 @@ import okhttp3.Response;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -219,26 +219,15 @@ public class RawCustomResourceOperationsImpl extends OperationSupport {
     return labelQueryBuilder.toString();
   }
 
-  private Map<String, Object> makeCall(String url, String body, HttpCallMethod callMethod) throws RuntimeException {
-    try {
-      Response response;
-      if(body == null) {
-        response = client.newCall(getRequest(url, callMethod)).execute();
-      }
-      else {
-        response = client.newCall(getRequest(url, body, callMethod)).execute();
-      }
-      if(response.code() != HttpURLConnection.HTTP_NOT_FOUND &&
-         response.code() != HttpURLConnection.HTTP_SERVER_ERROR &&
-         response.code() != HttpURLConnection.HTTP_BAD_REQUEST) {
+  private Map<String, Object> makeCall(String url, String body, HttpCallMethod callMethod) {
+    Request request = (body == null) ? getRequest(url, callMethod) : getRequest(url, body, callMethod);
+    try (Response response = client.newCall(request).execute()) {
+      if (response.isSuccessful()) {
         return objectMapper.readValue(response.body().string(), HashMap.class);
       } else {
-        if(response.body() != null) {
-          throw new KubernetesClientException(response.body().string());
-        } else {
-          response.close();
-          throw new KubernetesClientException(response.message());
-        }
+        String message = String.format("Error while performing the call to %s. Response code: %s", url, response.code());
+        Status status = createStatus(response);
+        throw new KubernetesClientException(message, response.code(), status);
       }
     } catch(Exception e) {
       throw KubernetesClientException.launderThrowable(e);
