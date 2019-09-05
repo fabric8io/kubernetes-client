a9e91e9ee172382c090816b975d7bac56c22800a
Jorge Sartori <jorge.rocha@serpro.gov.br>
2019-09-05 20:19:53 -0300

    iss_1688 - Formatting correct

--- kubernetes-server-mock/src/main/java/io/fabric8/kubernetes/client/server/mock/KubernetesAttributesExtractor.java
+++ kubernetes-server-mock/src/main/java/io/fabric8/kubernetes/client/server/mock/KubernetesAttributesExtractor.java
@@ -37,188 +37,188 @@ import okhttp3.HttpUrl;
 
 public class KubernetesAttributesExtractor implements AttributeExtractor<HasMetadata> {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(KubernetesAttributesExtractor.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(KubernetesAttributesExtractor.class);
 
-	public static final String KEY = "key";
-	public static final String KIND = "kind";
-	public static final String NAME = "name";
-	public static final String NAMESPACE = "namespace";
-	public static final String VALUE = "value";
+  public static final String KEY = "key";
+  public static final String KIND = "kind";
+  public static final String NAME = "name";
+  public static final String NAMESPACE = "namespace";
+  public static final String VALUE = "value";
 
-	private static final String API_GROUP = "/o?api(s/[a-zA-Z0-9-_.]+)?";
-	private static final String VERSION_GROUP = "(/(?<version>[a-zA-Z0-9-_]+))?";
-	private static final String KIND_GROUP = "/(?<kind>[^/?]+)";
-	private static final String NAME_GROUP = "(/(?<name>[^/?]+))?";
-	private static final String NAMESPACE_GROUP = "(/namespaces/(?<namespace>[^/]+))?";
-	private static final String END_GROUP = "[^ /]*";
+  private static final String API_GROUP = "/o?api(s/[a-zA-Z0-9-_.]+)?";
+  private static final String VERSION_GROUP = "(/(?<version>[a-zA-Z0-9-_]+))?";
+  private static final String KIND_GROUP = "/(?<kind>[^/?]+)";
+  private static final String NAME_GROUP = "(/(?<name>[^/?]+))?";
+  private static final String NAMESPACE_GROUP = "(/namespaces/(?<namespace>[^/]+))?";
+  private static final String END_GROUP = "[^ /]*";
 
-	protected static final Pattern PATTERN = Pattern
-			.compile(API_GROUP + VERSION_GROUP + NAMESPACE_GROUP + KIND_GROUP + NAME_GROUP + END_GROUP);
+  protected static final Pattern PATTERN = Pattern.compile(API_GROUP + VERSION_GROUP + NAMESPACE_GROUP + KIND_GROUP + NAME_GROUP + END_GROUP);
 
-	private static final String LABEL_KEY_PREFIX = "labels:";
-	private static final String KEY_GROUP = "(?<key>[a-zA-Z0-9-_./]+)";
-	// Matches a==b and a=b but not a!=b.
-	private static final String EQUALITY_GROUP = "(==|(?<!!)=)";
-	// Matches a!=b but not a==b or a=b .
-	private static final String NOT_EQUALITY_GROUP = "(!=)";
-	private static final String VALUE_GROUP = "(?<value>[a-zA-Z0-9-_.]+)";
-	private static final Pattern LABEL_REQUIREMENT_EQUALITY = Pattern.compile(KEY_GROUP + EQUALITY_GROUP + VALUE_GROUP);
-	private static final Pattern LABEL_REQUIREMENT_NOT_EQUALITY = Pattern
-			.compile(KEY_GROUP + NOT_EQUALITY_GROUP + VALUE_GROUP);
+  private static final String LABEL_KEY_PREFIX = "labels:";
+  private static final String KEY_GROUP = "(?<key>[a-zA-Z0-9-_./]+)";
+  // Matches a==b and a=b but not a!=b.
+  private static final String EQUALITY_GROUP = "(==|(?<!!)=)";
+  // Matches a!=b but not a==b or a=b .
+  private static final String NOT_EQUALITY_GROUP = "(!=)";
+  private static final String VALUE_GROUP = "(?<value>[a-zA-Z0-9-_.]+)";
+  private static final Pattern LABEL_REQUIREMENT_EQUALITY = Pattern.compile(KEY_GROUP + EQUALITY_GROUP + VALUE_GROUP);
+  private static final Pattern LABEL_REQUIREMENT_NOT_EQUALITY = Pattern.compile(KEY_GROUP + NOT_EQUALITY_GROUP + VALUE_GROUP);
 
-	// These elements are added to the path/query fragment so that it can be parsed
-	// by HttpUrl. Their
-	// values are not important, HttpUrl expects the scheme to be http or https.
-	private static final String SCHEME = "http";
-	private static final String HOST = "localhost";
+  // These elements are added to the path/query fragment so that it can be parsed by HttpUrl. Their
+  // values are not important, HttpUrl expects the scheme to be http or https.
+  private static final String SCHEME = "http";
+  private static final String HOST = "localhost";
 
-	private HttpUrl parseUrlFromPathAndQuery(String s) {
-		if (!s.startsWith("/")) {
-			s = "/" + s;
-		}
-		return HttpUrl.parse(String.format("%s://%s%s", SCHEME, HOST, s));
-	}
+  private HttpUrl parseUrlFromPathAndQuery(String s) {
+    if (!s.startsWith("/")) {
+      s = "/" + s;
+    }
+    return HttpUrl.parse(String.format("%s://%s%s", SCHEME, HOST, s));
+  }
 
-	@Override
-	public AttributeSet fromPath(String s) {
-		if (s == null || s.isEmpty()) {
-			return new AttributeSet();
-		}
+  @Override
+  public AttributeSet fromPath(String s) {
+    if (s == null || s.isEmpty()) {
+      return new AttributeSet();
+    }
 
-		// Get paths
-		HttpUrl url = parseUrlFromPathAndQuery(s);
-		Matcher m = PATTERN.matcher(url.encodedPath());
-		if (m.matches()) {
-			AttributeSet set = extract(m);
-			set = AttributeSet.merge(set, extractQueryParameters(url));
-			LOGGER.debug("fromPath {} : {}", s, set);
-			return set;
-		}
-		return new AttributeSet();
-	}
+    //Get paths
+    HttpUrl url = parseUrlFromPathAndQuery(s);
+    Matcher m = PATTERN.matcher(url.encodedPath());
+    if (m.matches()) {
+      AttributeSet set = extract(m);
+      set = AttributeSet.merge(set, extractQueryParameters(url));
+      LOGGER.debug("fromPath {} : {}", s, set);
+      return set;
+    }
+    return new AttributeSet();
+  }
 
-	@Override
-	public AttributeSet fromResource(String s) {
-		if (s == null || s.isEmpty()) {
-			return new AttributeSet();
-		}
+  @Override
+  public AttributeSet fromResource(String s) {
+    if (s == null || s.isEmpty()) {
+      return new AttributeSet();
+    }
 
-		HasMetadata h = toKubernetesResource(s);
-		if (h != null) {
-			return extract(h);
-		}
+    HasMetadata h = toKubernetesResource(s);
+    if (h != null) {
+      return extract(h);
+    }
 
-		return new AttributeSet();
-	}
+    return new AttributeSet();
+  }
 
-	@Override
-	public AttributeSet extract(String s) {
-		if (s == null || s.isEmpty()) {
-			return new AttributeSet();
-		}
+  @Override
+  public AttributeSet extract(String s) {
+    if (s == null || s.isEmpty()) {
+      return new AttributeSet();
+    }
 
-		HasMetadata h = toKubernetesResource(s);
-		if (h != null) {
-			return extract(h);
-		}
+    HasMetadata h = toKubernetesResource(s);
+    if (h != null) {
+      return extract(h);
+    }
 
-		return fromPath(s);
-	}
+    return fromPath(s);
+  }
 
-	@Override
-	public AttributeSet extract(HasMetadata o) {
-		AttributeSet attributes = new AttributeSet();
-		if (!Utils.isNullOrEmpty(o.getKind())) {
-			attributes = attributes.add(new Attribute(KIND, o.getKind().toLowerCase()));
-		}
+  @Override
+  public AttributeSet extract(HasMetadata o) {
+    AttributeSet attributes = new AttributeSet();
+    if (!Utils.isNullOrEmpty(o.getKind())) {
+      attributes = attributes.add(new Attribute(KIND, o.getKind().toLowerCase()));
+    }
 
-		if (!Utils.isNullOrEmpty(o.getMetadata().getName())) {
-			attributes = attributes.add(new Attribute(NAME, o.getMetadata().getName()));
-		}
+    if (!Utils.isNullOrEmpty(o.getMetadata().getName())) {
+      attributes = attributes.add(new Attribute(NAME, o.getMetadata().getName()));
+    }
 
-		if (!Utils.isNullOrEmpty(o.getMetadata().getNamespace())) {
-			attributes = attributes.add(new Attribute(NAMESPACE, o.getMetadata().getNamespace()));
-		}
+    if (!Utils.isNullOrEmpty(o.getMetadata().getNamespace())) {
+      attributes = attributes.add(new Attribute(NAMESPACE, o.getMetadata().getNamespace()));
+    }
 
-		if (o.getMetadata().getLabels() != null) {
-			for (Map.Entry<String, String> label : o.getMetadata().getLabels().entrySet()) {
-				attributes = attributes.add(new Attribute(LABEL_KEY_PREFIX + label.getKey(), label.getValue()));
-			}
-		}
-		return attributes;
-	}
+    if (o.getMetadata().getLabels() != null) {
+      for (Map.Entry<String, String> label : o.getMetadata().getLabels().entrySet()) {
+        attributes = attributes.add(new Attribute(LABEL_KEY_PREFIX + label.getKey(), label.getValue()));
+      }
+    }
+    return attributes;
+  }
 
-	private static AttributeSet extract(Matcher m) {
-		AttributeSet attributes = new AttributeSet();
-		if (m.matches()) {
-			String kind = m.group(KIND);
-			if (!Utils.isNullOrEmpty(kind)) {
+  private static AttributeSet extract(Matcher m) {
+    AttributeSet attributes = new AttributeSet();
+    if (m.matches()) {
+      String kind = m.group(KIND);
+      if (!Utils.isNullOrEmpty(kind)) {
 
-				// Poor mans to singular.
-				// Special Case for PodSecurityPolicies and NetworkPolicies because
-				// we need to return PodSecurityPolicy and NetworkPolicy respectively
-				// because it is returning PodSecurityPolicie and NetworkPolicie now
-				// Right now not adding generalised case of "ies" because it may break other
-				// resource not sure
+        //Poor mans to singular.
+        //Special Case for PodSecurityPolicies and NetworkPolicies because
+        //we need to return PodSecurityPolicy and NetworkPolicy respectively
+        //because it is returning PodSecurityPolicie and NetworkPolicie now
+        //Right now not adding generalised case of "ies" because it may break other resource not sure
 
-				if (kind.endsWith("ses")) {
-					kind = kind.substring(0, kind.length() - 2);
-				} else if (kind.equalsIgnoreCase("PodSecurityPolicies") || kind.equalsIgnoreCase("NetworkPolicies")) {
-					kind = kind.substring(0, kind.length() - 3) + "y";
-				} else if (kind.equalsIgnoreCase("securityContextConstraints") || kind.equalsIgnoreCase("endpoints")) {
-					// do nothing
-					// because its a case which is ending with s but its name is
-					// like that, it is not plural
-				} else if (kind.endsWith("s")) {
-					kind = kind.substring(0, kind.length() - 1);
-				}
-				attributes = attributes.add(new Attribute(KIND, kind));
-			}
+        if (kind.endsWith("ses")) {
+          kind = kind.substring(0, kind.length() - 2);
+        }
+        else if (kind.equalsIgnoreCase("PodSecurityPolicies") ||
+          kind.equalsIgnoreCase("NetworkPolicies")){
+          kind = kind.substring(0,kind.length() - 3) + "y";
+        }
+        else if (kind.equalsIgnoreCase("securityContextConstraints") ||
+          kind.equalsIgnoreCase("endpoints")){
+          // do nothing
+          // because its a case which is ending with s but its name is
+          // like that, it is not plural
+        }
+        else if (kind.endsWith("s")) {
+          kind = kind.substring(0, kind.length() - 1);
+        }
+        attributes = attributes.add(new Attribute(KIND, kind));
+      }
 
-			String namespace = m.group(NAMESPACE);
-			if (!Utils.isNullOrEmpty(namespace)) {
-				attributes = attributes.add(new Attribute(NAMESPACE, namespace));
-			}
+      String namespace = m.group(NAMESPACE);
+      if (!Utils.isNullOrEmpty(namespace)) {
+        attributes = attributes.add(new Attribute(NAMESPACE, namespace));
+      }
 
-			try {
-				String name = m.group(NAME);
-				if (!Utils.isNullOrEmpty(name)) {
-					attributes = attributes.add(new Attribute(NAME, name));
-				}
-			} catch (IllegalArgumentException e) {
-				// group is missing, which is perfectly valid for create, update etc requests.
-			}
-		}
-		return attributes;
-	}
+      try {
+        String name = m.group(NAME);
+        if (!Utils.isNullOrEmpty(name)) {
+          attributes = attributes.add(new Attribute(NAME, name));
+        }
+      } catch (IllegalArgumentException e) {
+        //group is missing, which is perfectly valid for create, update etc requests.
+      }
+    }
+    return attributes;
+  }
 
-	private static AttributeSet extractQueryParameters(HttpUrl url) {
-		AttributeSet attributes = new AttributeSet();
-		String labelSelector = url.queryParameter("labelSelector");
-		if (labelSelector != null) {
-			for (String requirement : labelSelector.split(",")) {
-				Matcher m = LABEL_REQUIREMENT_EQUALITY.matcher(requirement);
-				if (m.matches()) {
-					attributes = attributes.add(new Attribute(LABEL_KEY_PREFIX + m.group(KEY), m.group(VALUE)));
-				} else {
-					m = LABEL_REQUIREMENT_NOT_EQUALITY.matcher(requirement);
-					if (m.matches()) {
-						attributes = attributes
-								.add(new Attribute(LABEL_KEY_PREFIX + m.group(KEY), m.group(VALUE), WITHOUT));
-					} else {
-						LOGGER.warn("Ignoring unsupported label requirement: {}", requirement);
-					}
-				}
-			}
-		}
-		return attributes;
-	}
+  private static AttributeSet extractQueryParameters(HttpUrl url) {
+    AttributeSet attributes = new AttributeSet();
+    String labelSelector = url.queryParameter("labelSelector");
+    if (labelSelector != null) {
+      for (String requirement : labelSelector.split(",")) {
+        Matcher m = LABEL_REQUIREMENT_EQUALITY.matcher(requirement);
+        if (m.matches()) {
+          attributes = attributes.add(new Attribute(LABEL_KEY_PREFIX + m.group(KEY), m.group(VALUE)));
+        } else {
+          m = LABEL_REQUIREMENT_NOT_EQUALITY.matcher(requirement);
+          if (m.matches()) {
+            attributes = attributes.add(new Attribute(LABEL_KEY_PREFIX + m.group(KEY), m.group(VALUE), WITHOUT));
+          } else {
+            LOGGER.warn("Ignoring unsupported label requirement: {}", requirement);
+          }
+        }
+      }
+    }
+    return attributes;
+  }
 
-	private static HasMetadata toKubernetesResource(String s) {
-		try (InputStream stream = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8.name()))) {
-			return Serialization.unmarshal(stream);
-		} catch (Exception e) {
-			return null;
-		}
-	}
+  private static HasMetadata toKubernetesResource(String s) {
+    try (InputStream stream = new ByteArrayInputStream(s.getBytes(StandardCharsets.UTF_8.name()))) {
+      return Serialization.unmarshal(stream);
+    } catch (Exception e) {
+      return null;
+    }
+  }
 }
