f1b1ce9e166f1ffda8fd551f870b41f59db1b4ae
Vincent Latombe <vincent@latombe.net>
2019-09-16 10:34:48 +0200

    Add a unit test

---
+++ openshift-client/src/test/java/io/fabric8/openshift/client/HandlersTest.java
@@ -0,0 +1,67 @@
+package io.fabric8.openshift.client;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.client.ResourceHandler;
+import io.fabric8.openshift.api.model.Build;
+import io.fabric8.openshift.api.model.BuildConfig;
+import io.fabric8.openshift.api.model.DeploymentConfig;
+import io.fabric8.openshift.api.model.Group;
+import io.fabric8.openshift.api.model.Identity;
+import io.fabric8.openshift.api.model.Image;
+import io.fabric8.openshift.api.model.ImageStream;
+import io.fabric8.openshift.api.model.ImageStreamTag;
+import io.fabric8.openshift.api.model.NetNamespace;
+import io.fabric8.openshift.api.model.OAuthAccessToken;
+import io.fabric8.openshift.api.model.OAuthAuthorizeToken;
+import io.fabric8.openshift.api.model.OAuthClient;
+import io.fabric8.openshift.api.model.Project;
+import io.fabric8.openshift.api.model.Route;
+import io.fabric8.openshift.api.model.SecurityContextConstraints;
+import io.fabric8.openshift.api.model.User;
+import io.fabric8.openshift.client.handlers.BuildConfigHandler;
+import io.fabric8.openshift.client.handlers.BuildHandler;
+import io.fabric8.openshift.client.handlers.DeploymentConfigHandler;
+import io.fabric8.openshift.client.handlers.GroupHandler;
+import io.fabric8.openshift.client.handlers.IdentityHandler;
+import io.fabric8.openshift.client.handlers.ImageHandler;
+import io.fabric8.openshift.client.handlers.ImageStreamHandler;
+import io.fabric8.openshift.client.handlers.ImageStreamTagHandler;
+import io.fabric8.openshift.client.handlers.NetNamespaceHandler;
+import io.fabric8.openshift.client.handlers.OAuthAccessTokenHandler;
+import io.fabric8.openshift.client.handlers.OAuthAuthorizeTokenHandler;
+import io.fabric8.openshift.client.handlers.OAuthClientHandler;
+import io.fabric8.openshift.client.handlers.ProjectHandler;
+import io.fabric8.openshift.client.handlers.RouteHandler;
+import io.fabric8.openshift.client.handlers.SecurityContextConstraintsHandler;
+import io.fabric8.openshift.client.handlers.UserHandler;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HandlersTest {
+
+  @Test
+  public void checkHandlers() {
+    checkHandler(new BuildConfig(), new BuildConfigHandler());
+    checkHandler(new Build(), new BuildHandler());
+    checkHandler(new DeploymentConfig(), new DeploymentConfigHandler());
+    checkHandler(new Group(), new GroupHandler());
+    checkHandler(new Identity(), new IdentityHandler());
+    checkHandler(new Image(), new ImageHandler());
+    checkHandler(new ImageStream(), new ImageStreamHandler());
+    checkHandler(new ImageStreamTag(), new ImageStreamTagHandler());
+    checkHandler(new NetNamespace(), new NetNamespaceHandler());
+    checkHandler(new OAuthAccessToken(), new OAuthAccessTokenHandler());
+    checkHandler(new OAuthAuthorizeToken(), new OAuthAuthorizeTokenHandler());
+    checkHandler(new OAuthClient(), new OAuthClientHandler());
+    checkHandler(new Project(), new ProjectHandler());
+    checkHandler(new Route(), new RouteHandler());
+    checkHandler(new SecurityContextConstraints(), new SecurityContextConstraintsHandler());
+    checkHandler(new User(), new UserHandler());
+  }
+
+  private void checkHandler(HasMetadata hasMetadata, ResourceHandler handler) {
+    assertEquals(hasMetadata.getKind().toLowerCase(), handler.getKind().toLowerCase());
+    assertEquals(hasMetadata.getApiVersion().toLowerCase(), handler.getApiVersion().toLowerCase());
+  }
+}
---
+++ openshift-client/src/test/java/io/fabric8/openshift/client/Handlersv4Test.java
@@ -0,0 +1,66 @@
+package io.fabric8.openshift.client;
+
+import io.fabric8.kubernetes.api.model.v4_0.HasMetadata;
+import io.fabric8.kubernetes.clnt.v4_0.ResourceHandler;
+import io.fabric8.openshift.api.model.v4_0.Build;
+import io.fabric8.openshift.api.model.v4_0.BuildConfig;
+import io.fabric8.openshift.api.model.v4_0.DeploymentConfig;
+import io.fabric8.openshift.api.model.v4_0.Group;
+import io.fabric8.openshift.api.model.v4_0.ImageStream;
+import io.fabric8.openshift.api.model.v4_0.ImageStreamTag;
+import io.fabric8.openshift.api.model.v4_0.OAuthAccessToken;
+import io.fabric8.openshift.api.model.v4_0.OAuthAuthorizeToken;
+import io.fabric8.openshift.api.model.v4_0.OAuthClient;
+import io.fabric8.openshift.api.model.v4_0.PolicyBinding;
+import io.fabric8.openshift.api.model.v4_0.Project;
+import io.fabric8.openshift.api.model.v4_0.RoleBinding;
+import io.fabric8.openshift.api.model.v4_0.Route;
+import io.fabric8.openshift.api.model.v4_0.SecurityContextConstraints;
+import io.fabric8.openshift.api.model.v4_0.Template;
+import io.fabric8.openshift.api.model.v4_0.User;
+import io.fabric8.openshift.clnt.v4_0.handlers.BuildConfigHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.BuildHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.DeploymentConfigHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.GroupHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.ImageStreamHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.ImageStreamTagHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.OAuthAccessTokenHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.OAuthAuthorizeTokenHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.OAuthClientHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.PolicyBindingHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.ProjectHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.RoleBindingHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.RouteHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.SecurityContextConstraintsHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.TemplateHandler;
+import io.fabric8.openshift.clnt.v4_0.handlers.UserHandler;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Handlersv4Test {
+
+  @Test
+  public void checkHandlers() {
+    checkHandler(new BuildConfig(), new BuildConfigHandler());
+    checkHandler(new Build(), new BuildHandler());
+    checkHandler(new DeploymentConfig(), new DeploymentConfigHandler());
+    checkHandler(new Group(), new GroupHandler());
+    checkHandler(new ImageStream(), new ImageStreamHandler());
+    checkHandler(new ImageStreamTag(), new ImageStreamTagHandler());
+    checkHandler(new OAuthAccessToken(), new OAuthAccessTokenHandler());
+    checkHandler(new OAuthAuthorizeToken(), new OAuthAuthorizeTokenHandler());
+    checkHandler(new OAuthClient(), new OAuthClientHandler());
+    checkHandler(new PolicyBinding(), new PolicyBindingHandler());
+    checkHandler(new Project(), new ProjectHandler());
+    checkHandler(new RoleBinding(), new RoleBindingHandler());
+    checkHandler(new Route(), new RouteHandler());
+    checkHandler(new SecurityContextConstraints(), new SecurityContextConstraintsHandler());
+    checkHandler(new Template(), new TemplateHandler());
+    checkHandler(new User(), new UserHandler());
+  }
+
+  private void checkHandler(HasMetadata hasMetadata, ResourceHandler handler) {
+    assertEquals(hasMetadata.getKind().toLowerCase(), handler.getKind().toLowerCase());
+  }
+}
