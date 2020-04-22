e26571f89d62917da307fcfc74c589ab2299ff6f
Andre Dietisheim <adietish@redhat.com>
2020-04-22 15:39:24 +0200

    KubernetesDeserialier loads mappings via current thread (#2109)
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- kubernetes-model/kubernetes-model-core/src/main/java/io/fabric8/kubernetes/internal/KubernetesDeserializer.java
+++ kubernetes-model/kubernetes-model-core/src/main/java/io/fabric8/kubernetes/internal/KubernetesDeserializer.java
@@ -21,16 +21,19 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.ServiceLoader;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.fabric8.kubernetes.api.KubernetesResourceMappingProvider;
 import io.fabric8.kubernetes.api.model.HasMetadata;
@@ -41,104 +44,59 @@ public class KubernetesDeserializer extends JsonDeserializer<KubernetesResource>
 
     private static final String KIND = "kind";
     private static final String API_VERSION = "apiVersion";
-    private static final String KEY_SEPARATOR = "#";
 
-    private static final Map<String, Class<? extends KubernetesResource>> MAP = new ConcurrentHashMap<>();
-    private static final String[] PACKAGES = {
-      "io.fabric8.kubernetes.api.model.",
-      "io.fabric8.kubernetes.api.model.admissionregistration.",
-      "io.fabric8.kubernetes.api.model.apiextensions.",
-      "io.fabric8.kubernetes.api.model.apps.",
-      "io.fabric8.kubernetes.api.model.authentication.",
-      "io.fabric8.kubernetes.api.model.authorization.",
-      "io.fabric8.kubernetes.api.model.batch.",
-      "io.fabric8.kubernetes.api.model.certificates.",
-      "io.fabric8.kubernetes.api.model.coordination.",
-      "io.fabric8.kubernetes.api.model.discovery.",
-      "io.fabric8.kubernetes.api.model.extensions.",
-      "io.fabric8.kubernetes.api.model.events.",
-      "io.fabric8.kubernetes.api.model.networking.",
-      "io.fabric8.kubernetes.api.model.policy.",
-      "io.fabric8.kubernetes.api.model.rbac.",
-      "io.fabric8.kubernetes.api.model.storage.",
-      "io.fabric8.kubernetes.api.model.scheduling.",
-      "io.fabric8.kubernetes.api.model.settings.",
-      "io.fabric8.openshift.api.model."
-    };
+    private static final Mapping mapping = new Mapping();
 
-    static {
-        //Use service loader to load extension types.
-        for (KubernetesResourceMappingProvider provider : ServiceLoader.load(KubernetesResourceMappingProvider.class)) {
-            for (Map.Entry<String, Class<? extends KubernetesResource>> entry : provider.getMappings().entrySet()) {
-                String key = entry.getKey();
-                Class<? extends KubernetesResource> value = entry.getValue();
-                //If the model is shaded (which is as part of kubernetes-client uberjar) this is going to cause conflicts.
-                //This is why we NEED TO filter out incompatible resources.
-                if (KubernetesResource.class.isAssignableFrom(value)) {
-                        MAP.put(key, value);
+    @Override
+    public KubernetesResource deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        JsonNode node = jp.readValueAsTree();
+        if (node.isObject()) {
+            return fromObjectNode(jp, node);
+        } else if (node.isArray()) {
+            return fromArrayNode(jp, node);
+        } else {
+            return null;
+        }
+    }
+
+    private KubernetesResource fromArrayNode(JsonParser jp, JsonNode node) throws IOException {
+        Iterator<JsonNode> iterator = node.elements();
+        List<HasMetadata> list = new ArrayList<>();
+        while (iterator.hasNext()) {
+            JsonNode jsonNode = iterator.next();
+            if (jsonNode.isObject()) {
+                KubernetesResource resource = fromObjectNode(jp, jsonNode);
+                if (resource instanceof HasMetadata) {
+                    list.add((HasMetadata)resource);
                 }
             }
         }
+        return new KubernetesListBuilder().withItems(list).build();
     }
 
-    @Override
-    public KubernetesResource deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        JsonNode node = jp.readValueAsTree();
-        if (node instanceof ObjectNode) {
-          return fromObjectNode(jp, (ObjectNode) node);
-        } else if (node instanceof ArrayNode) {
-          ArrayNode arrayNode = (ArrayNode) node;
-          Iterator<JsonNode> iterator = arrayNode.elements();
-          List<HasMetadata> list = new ArrayList();
-          while (iterator.hasNext()) {
-            JsonNode jsonNode = iterator.next();
-            if (jsonNode instanceof ObjectNode) {
-              KubernetesResource resource = fromObjectNode(jp, (ObjectNode) jsonNode);
-              if (resource instanceof HasMetadata) {
-                list.add((HasMetadata)resource);
-              }
+    private static KubernetesResource fromObjectNode(JsonParser jp, JsonNode node) throws IOException {
+        String key = getKey(node);
+        if (key != null) {
+            Class<? extends KubernetesResource> resourceType = mapping.getForKey(key);
+            if (resourceType == null) {
+                throw JsonMappingException.from(jp,"No resource type found for:" + key);
+            } else if (KubernetesResource.class.isAssignableFrom(resourceType)){
+                return jp.getCodec().treeToValue(node, resourceType);
             }
-          }
-          return new KubernetesListBuilder().withItems(list).build();
         }
         return null;
-    }
-
-  private static KubernetesResource fromObjectNode(JsonParser jp, ObjectNode node) throws IOException, JsonProcessingException {
-      String key = getKey(node);
-      if (key != null) {
-        Class<? extends KubernetesResource> resourceType = getTypeForKey(key);
-        if (resourceType == null) {
-          throw JsonMappingException.from(jp,"No resource type found for:" + key);
-        } else {
-          return jp.getCodec().treeToValue(node, resourceType);
-        }
-      }
-    return null;
     }
 
     /**
      * Return a string representation of the key of the type: <version>#<kind>.
      */
-    private static String getKey(ObjectNode node) {
+    private static String getKey(JsonNode node) {
         JsonNode apiVersion = node.get(API_VERSION);
         JsonNode kind = node.get(KIND);
 
-        return getKey(apiVersion != null ? apiVersion.textValue() : null,
-                      kind != null ? kind.textValue() : null);
-    }
-    
-    /**
-     * Returns a composite key for apiVersion and kind.
-     */
-    private static String getKey(String apiVersion, String kind) {
-        if (kind == null) {
-            return null;
-        } else if (apiVersion == null) {
-            return kind;
-        } else {
-            return String.format("%s#%s", apiVersion, kind);
-        }
+        return mapping.createKey(
+                apiVersion != null ? apiVersion.textValue() : null,
+                kind != null ? kind.textValue() : null);
     }
 
     /**
@@ -152,42 +110,144 @@ public class KubernetesDeserializer extends JsonDeserializer<KubernetesResource>
      * Registers a Custom Resource Definition Kind
      */
     public static void registerCustomKind(String apiVersion, String kind, Class<? extends KubernetesResource> clazz) {
-        MAP.put(getKey(apiVersion, kind), clazz);
+        mapping.registerKind(apiVersion, kind, clazz);
     }
 
-    private static Class getTypeForKey(String key) {
-        Class<? extends KubernetesResource> result = MAP.get(key);
-        if (result == null) {
-            String name = key != null && key.contains(KEY_SEPARATOR) ?
-                key.substring(key.indexOf(KEY_SEPARATOR) + 1) :
-                key;
-            
-            result = getInternalTypeForName(name);
-        }
-
-        if (result != null) {
-            MAP.put(key, result);
-        }
-        return result;
+    /**
+     * Registers a Custom Resource Mapping Provider
+     */
+    public static void registerProvider(KubernetesResourceMappingProvider provider) {
+        mapping.registerProvider(provider);
     }
 
-    private static Class getInternalTypeForName(String name) {
+    static class Mapping {
 
-      for (String aPackage : PACKAGES) {
-        Class result = loadClassIfExists(aPackage + name);
-        if (result != null) {
-          return result;
+        private static final String KEY_SEPARATOR = "#";
+        private static final String[] PACKAGES = {
+                "io.fabric8.kubernetes.api.model.",
+                "io.fabric8.kubernetes.api.model.admissionregistration.",
+                "io.fabric8.kubernetes.api.model.apiextensions.",
+                "io.fabric8.kubernetes.api.model.apps.",
+                "io.fabric8.kubernetes.api.model.authentication.",
+                "io.fabric8.kubernetes.api.model.authorization.",
+                "io.fabric8.kubernetes.api.model.batch.",
+                "io.fabric8.kubernetes.api.model.certificates.",
+                "io.fabric8.kubernetes.api.model.coordination.",
+                "io.fabric8.kubernetes.api.model.discovery.",
+                "io.fabric8.kubernetes.api.model.extensions.",
+                "io.fabric8.kubernetes.api.model.events.",
+                "io.fabric8.kubernetes.api.model.networking.",
+                "io.fabric8.kubernetes.api.model.policy.",
+                "io.fabric8.kubernetes.api.model.rbac.",
+                "io.fabric8.kubernetes.api.model.storage.",
+                "io.fabric8.kubernetes.api.model.scheduling.",
+                "io.fabric8.kubernetes.api.model.settings.",
+                "io.fabric8.openshift.api.model."
+        };
+
+        private Map<String, Class<? extends KubernetesResource>> mappings = new ConcurrentHashMap<>();
+
+        Mapping() {
+        	registerAllProviders();
         }
-      }
+        
+        public Class<? extends KubernetesResource> getForKey(String key) {
+        	if (key == null) {
+        		return null;
+        	}
+        	Class<? extends KubernetesResource> clazz = mappings.get(key);
+        	if (clazz != null) {
+        		return clazz; 
+        	} 
+    		clazz = getInternalTypeForName(getClassName(key));
+    		if (clazz != null) {
+				mappings.put(key, clazz);
+			}
+    		return clazz;
+        }
 
-        return null;
-    } 
+        public void registerKind(String apiVersion, String kind, Class<? extends KubernetesResource> clazz) {
+        	mappings.put(createKey(apiVersion, kind), clazz);
+        }
 
-    private static Class loadClassIfExists(String className) {
-        try {
-            return KubernetesDeserializer.class.getClassLoader().loadClass(className);
-        } catch (Throwable t) {
+        public void registerProvider(KubernetesResourceMappingProvider provider) {
+            if (provider == null) {
+                return;
+            }
+            Map<String, Class<? extends KubernetesResource>> providerMappings = provider.getMappings().entrySet().stream()
+                    //If the model is shaded (which is as part of kubernetes-client uberjar) this is going to cause conflicts.
+                    //This is why we NEED TO filter out incompatible resources.
+                    .filter(entry -> KubernetesResource.class.isAssignableFrom(entry.getValue()))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+            mappings.putAll(providerMappings);
+        }
+
+        /**
+         * Returns a composite key for apiVersion and kind.
+         */
+        String createKey(String apiVersion, String kind) {
+            if (kind == null) {
+                return null;
+            } else if (apiVersion == null) {
+                return kind;
+            } else {
+                return apiVersion + KEY_SEPARATOR + kind;
+            }
+        }
+
+        private void registerAllProviders() {
+            getAllMappingProviders().forEach(this::registerProvider);
+        }
+
+        Stream<KubernetesResourceMappingProvider> getAllMappingProviders() {
+            //Use service loader to load extension types.
+            Iterable<KubernetesResourceMappingProvider> currentThreadClassLoader =
+                    () -> ServiceLoader.load(KubernetesResourceMappingProvider.class, Thread.currentThread().getContextClassLoader())
+                            .iterator();
+            Iterable<KubernetesResourceMappingProvider> classClassLoader =
+                    () -> ServiceLoader.load(KubernetesResourceMappingProvider.class, KubernetesDeserializer.class.getClassLoader())
+                            .iterator();
+            return Stream.concat(
+                    StreamSupport.stream(currentThreadClassLoader.spliterator(), false),
+                    StreamSupport.stream(classClassLoader.spliterator(), false))
+                    .filter(distinctByClassName(KubernetesResourceMappingProvider::getClass));
+        }
+
+        private String getClassName(String key) {
+            if (key != null && key.contains(KEY_SEPARATOR)) {
+                return key.substring(key.indexOf(KEY_SEPARATOR) + 1);
+            } else {
+                return key;
+            }
+        }
+
+        private Class<? extends KubernetesResource> getInternalTypeForName(String name) {
+            for (String aPackage : PACKAGES) {
+                Class<? extends KubernetesResource> result = loadClassIfExists(aPackage + name);
+                if (result != null) {
+                    return result;
+                }
+            }
             return null;
         }
+
+        private Class<? extends KubernetesResource> loadClassIfExists(String className) {
+            try {
+            	Class<?> clazz = KubernetesDeserializer.class.getClassLoader().loadClass(className);
+                if (!KubernetesResource.class.isAssignableFrom(clazz)) {
+                    return null;
+                }
+                return (Class<? extends KubernetesResource>) clazz;
+            } catch (Exception t) {
+                return null;
+            }
+        }
+
+        private Predicate<KubernetesResourceMappingProvider> distinctByClassName(
+                Function<KubernetesResourceMappingProvider, Class<? extends KubernetesResourceMappingProvider>> mapperProvider) {
+            Set<String> existing = ConcurrentHashMap.newKeySet();
+            return provider -> existing.add(mapperProvider.apply(provider).getName());
+        }
+
     }
 }
