e7ee1e38530c57bcdde78fd73f66fab861cd2888
Lionel <pangmingshi@163.com>
2020-05-11 10:53:58 +0800

    remove generator vendor

--- extensions/kudo/generator/vendor/github.com/kudobuilder/kudo/pkg/kudoctl/packages/verifier.go
+++
@@ -1,8 +0,0 @@
-package packages
-
-import "github.com/kudobuilder/kudo/pkg/kudoctl/verifier"
-
-// Verifier defines the interface for all parameter verifiers
-type Verifier interface {
-	Verify(pf *Files) verifier.Result
-}
