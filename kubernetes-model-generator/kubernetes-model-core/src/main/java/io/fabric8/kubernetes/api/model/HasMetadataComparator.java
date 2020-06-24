fdf2c5348de381c3d3052fdfb34051c72d64ea3f
Johannes Edmeier <johannes.edmeier@chaosmesh.com>
2020-06-24 13:47:23 +0200

    move HasMetadataComparator to exported package
    The openshift-model needs to make use of the HasMetadataComparator and therefore
    it should reside in an exported package
    
    fixes #2309

--- kubernetes-model-generator/kubernetes-model-core/src/main/java/io/fabric8/kubernetes/api/model/HasMetadataComparator.java
+++ kubernetes-model-generator/kubernetes-model-core/src/main/java/io/fabric8/kubernetes/api/model/HasMetadataComparator.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.fabric8.kubernetes.internal;
+package io.fabric8.kubernetes.api.model;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 
--- kubernetes-model-generator/kubernetes-model-core/src/main/java/io/fabric8/kubernetes/api/model/KubernetesList.java
+++ kubernetes-model-generator/kubernetes-model-core/src/main/java/io/fabric8/kubernetes/api/model/KubernetesList.java
@@ -19,12 +19,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import io.fabric8.kubernetes.internal.HasMetadataComparator;
 import io.sundr.builder.annotations.Buildable;
 import io.sundr.builder.annotations.Inline;
 
 import javax.annotation.Generated;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
