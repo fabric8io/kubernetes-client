9a3baa98eb15d9c83fc26aba596ecbb1327e4ce0
piomin <piotr.minkowski@gmail.com>
2020-06-30 20:39:45 +0200

    Adding extension for JUnit5 support

---
+++ kubernetes-server-mock/src/main/java/io/fabric8/kubernetes/client/server/mock/KubernetesMockServerExtension.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.fabric8.kubernetes.client.server.mock;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Optional;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.NamespacedKubernetesClient;
+import io.fabric8.mockwebserver.Context;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+
+public class KubernetesMockServerExtension implements AfterEachCallback, AfterAllCallback, BeforeEachCallback, BeforeAllCallback {
+
+  private KubernetesMockServer mock;
+  private NamespacedKubernetesClient client;
+
+  @Override
+  public void afterEach(ExtensionContext context) throws Exception {
+    mock.destroy();
+    client.close();
+  }
+
+  @Override
+  public void afterAll(ExtensionContext context) throws Exception {
+    mock.destroy();
+    client.close();
+  }
+
+  @Override
+  public void beforeEach(ExtensionContext context) throws Exception {
+    setKubernetesClientField(context, false);
+  }
+
+  @Override
+  public void beforeAll(ExtensionContext context) throws Exception {
+    setKubernetesClientField(context, true);
+  }
+
+  private void setKubernetesClientField(ExtensionContext context, boolean isStatic) throws IllegalAccessException {
+    Optional<Class<?>> optClass = context.getTestClass();
+    if (optClass.isPresent()) {
+      Field[] fields = optClass.get().getDeclaredFields();
+      for (Field f : fields) {
+        if (f.getType() == KubernetesClient.class && Modifier.isStatic(f.getModifiers()) == isStatic) {
+          EnableKubernetesMockClient a = optClass.get().getAnnotation(EnableKubernetesMockClient.class);
+          mock = a.crud()
+                  ? new KubernetesMockServer(new Context(), new MockWebServer(), new HashMap<>(), new KubernetesCrudDispatcher(), a.https())
+                  : new KubernetesMockServer(a.https());
+          mock.init();
+          client = mock.createClient();
+          f.setAccessible(true);
+          if (isStatic) {
+            f.set(null, client);
+          } else if (context.getTestInstance().isPresent()) {
+            f.set(context.getTestInstance().get(), client);
+          }
+        }
+      }
+    }
+  }
+}
