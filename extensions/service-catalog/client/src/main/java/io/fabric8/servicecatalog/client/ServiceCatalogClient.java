10c3f023f88f12970174db889c90f782bc7dae66
Rohan Kumar <rohaan@redhat.com>
2020-08-21 19:33:09 +0530

    Chore: Add JavaDocs to all main client entrypoint extension interfaces

--- extensions/service-catalog/client/src/main/java/io/fabric8/servicecatalog/client/ServiceCatalogClient.java
+++ extensions/service-catalog/client/src/main/java/io/fabric8/servicecatalog/client/ServiceCatalogClient.java
@@ -20,6 +20,12 @@ import io.fabric8.kubernetes.client.Client;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
+import io.fabric8.servicecatalog.api.model.ClusterServiceBroker;
+import io.fabric8.servicecatalog.api.model.ClusterServiceBrokerList;
+import io.fabric8.servicecatalog.api.model.ClusterServiceClass;
+import io.fabric8.servicecatalog.api.model.ClusterServiceClassList;
+import io.fabric8.servicecatalog.api.model.ClusterServicePlan;
+import io.fabric8.servicecatalog.api.model.ClusterServicePlanList;
 import io.fabric8.servicecatalog.api.model.DoneableClusterServiceBroker;
 import io.fabric8.servicecatalog.api.model.DoneableClusterServiceClass;
 import io.fabric8.servicecatalog.api.model.DoneableClusterServicePlan;
@@ -28,25 +34,81 @@ import io.fabric8.servicecatalog.api.model.DoneableServiceBroker;
 import io.fabric8.servicecatalog.api.model.DoneableServiceClass;
 import io.fabric8.servicecatalog.api.model.DoneableServiceInstance;
 import io.fabric8.servicecatalog.api.model.DoneableServicePlan;
-import io.fabric8.servicecatalog.client.internal.*;
-import io.fabric8.servicecatalog.api.model.*;
+import io.fabric8.servicecatalog.api.model.ServiceBinding;
+import io.fabric8.servicecatalog.api.model.ServiceBindingList;
+import io.fabric8.servicecatalog.api.model.ServiceBroker;
+import io.fabric8.servicecatalog.api.model.ServiceBrokerList;
+import io.fabric8.servicecatalog.api.model.ServiceClass;
+import io.fabric8.servicecatalog.api.model.ServiceClassList;
+import io.fabric8.servicecatalog.api.model.ServiceInstance;
+import io.fabric8.servicecatalog.api.model.ServiceInstanceList;
+import io.fabric8.servicecatalog.api.model.ServicePlan;
+import io.fabric8.servicecatalog.api.model.ServicePlanList;
+import io.fabric8.servicecatalog.client.internal.ClusterServiceBrokerResource;
+import io.fabric8.servicecatalog.client.internal.ClusterServiceClassResource;
+import io.fabric8.servicecatalog.client.internal.ClusterServicePlanResource;
+import io.fabric8.servicecatalog.client.internal.ServiceBindingResource;
+import io.fabric8.servicecatalog.client.internal.ServiceInstanceResource;
 
+/**
+ * Main interface for Service Catalog Client.
+ */
 public interface ServiceCatalogClient extends Client {
 
-    NonNamespaceOperation<ClusterServiceBroker, ClusterServiceBrokerList, DoneableClusterServiceBroker, ClusterServiceBrokerResource> clusterServiceBrokers();
+  /**
+   * API entrypoint for ClusterServiceBroker(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return NonNamespaceOperation for ClusterServiceBroker class
+   */
+  NonNamespaceOperation<ClusterServiceBroker, ClusterServiceBrokerList, DoneableClusterServiceBroker, ClusterServiceBrokerResource> clusterServiceBrokers();
 
-    NonNamespaceOperation<ClusterServiceClass, ClusterServiceClassList, DoneableClusterServiceClass, ClusterServiceClassResource> clusterServiceClasses();
+  /**
+   * API entrypoint for ClusterServiceClass(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return NonNamespaceOperation for ClusterServiceClass class
+   */
+  NonNamespaceOperation<ClusterServiceClass, ClusterServiceClassList, DoneableClusterServiceClass, ClusterServiceClassResource> clusterServiceClasses();
 
-    NonNamespaceOperation<ClusterServicePlan, ClusterServicePlanList, DoneableClusterServicePlan, ClusterServicePlanResource> clusterServicePlans();
+  /**
+   * API entrypoint for ClusterServicePlan(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return NonNamespaceOperation for ClusterServicePlan class
+   */
+  NonNamespaceOperation<ClusterServicePlan, ClusterServicePlanList, DoneableClusterServicePlan, ClusterServicePlanResource> clusterServicePlans();
 
-    MixedOperation<ServiceInstance, ServiceInstanceList, DoneableServiceInstance, ServiceInstanceResource> serviceInstances();
+  /**
+   * API entrypoint for ServiceInstance(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return MixedOperation for ServiceInstance class
+   */
+  MixedOperation<ServiceInstance, ServiceInstanceList, DoneableServiceInstance, ServiceInstanceResource> serviceInstances();
 
-    MixedOperation<ServiceBinding, ServiceBindingList, DoneableServiceBinding, ServiceBindingResource> serviceBindings();
+  /**
+   * API entrypoint for ServiceBinding(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return MixedOperation for ServiceBinding class
+   */
+  MixedOperation<ServiceBinding, ServiceBindingList, DoneableServiceBinding, ServiceBindingResource> serviceBindings();
 
-    MixedOperation<ServiceBroker, ServiceBrokerList, DoneableServiceBroker, Resource<ServiceBroker, DoneableServiceBroker>> serviceBrokers();
+  /**
+   * API entrypoint for ServiceBroker(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return MixedOperation for ServiceBroker class
+   */
+  MixedOperation<ServiceBroker, ServiceBrokerList, DoneableServiceBroker, Resource<ServiceBroker, DoneableServiceBroker>> serviceBrokers();
 
-    MixedOperation<ServiceClass, ServiceClassList, DoneableServiceClass, Resource<ServiceClass, DoneableServiceClass>> serviceClasses();
+  /**
+   * API entrypoint for ServiceClass(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return MixedOperation for ServiceClass class
+   */
+  MixedOperation<ServiceClass, ServiceClassList, DoneableServiceClass, Resource<ServiceClass, DoneableServiceClass>> serviceClasses();
 
-    MixedOperation<ServicePlan, ServicePlanList, DoneableServicePlan, Resource<ServicePlan, DoneableServicePlan>> servicePlans();
+  /**
+   * API entrypoint for ServicePlan(servicecatalog.k8s.io/v1beta1)
+   *
+   * @return MixedOperation for ServicePlan class
+   */
+  MixedOperation<ServicePlan, ServicePlanList, DoneableServicePlan, Resource<ServicePlan, DoneableServicePlan>> servicePlans();
 
 }
--- extensions/tekton/client/src/main/java/io/fabric8/tekton/client/dsl/V1beta1APIGroupDSL.java
+++ extensions/tekton/client/src/main/java/io/fabric8/tekton/client/dsl/V1beta1APIGroupDSL.java
@@ -36,11 +36,39 @@ import io.fabric8.tekton.pipeline.v1beta1.TaskRun;
 import io.fabric8.tekton.pipeline.v1beta1.TaskRunList;
 
 public interface V1beta1APIGroupDSL extends Client {
+  /**
+   * API entrypoint for Pipeline(tekton.dev/v1beta1)
+   *
+   * @return MixedOperation for Pipeline class
+   */
   MixedOperation<Pipeline, PipelineList, DoneablePipeline, Resource<Pipeline, DoneablePipeline>> pipelines();
+
+  /**
+   * API entrypoint for PipelineRun(tekton.dev/v1beta1)
+   *
+   * @return MixedOperation for PipelineRun class
+   */
   MixedOperation<PipelineRun, PipelineRunList, DoneablePipelineRun, Resource<PipelineRun, DoneablePipelineRun>> pipelineRuns();
+
+  /**
+   * API entrypoint for Task(tekton.dev/v1beta1)
+   *
+   * @return MixedOperation for Task class
+   */
   MixedOperation<Task, TaskList, DoneableTask, Resource<Task, DoneableTask>> tasks();
+
+  /**
+   * API entrypoint for TaskRun(tekton.dev/v1beta1)
+   *
+   * @return MixedOperation for TaskRun class
+   */
   MixedOperation<TaskRun, TaskRunList, DoneableTaskRun, Resource<TaskRun, DoneableTaskRun>> taskRuns();
 
+  /**
+   * API entrypoint for ClusterTask(tekton.dev/v1beta1)
+   *
+   * @return MixedOperation for ClusterTask class
+   */
   NonNamespaceOperation<ClusterTask, ClusterTaskList, DoneableClusterTask, Resource<ClusterTask, DoneableClusterTask>> clusterTasks();
 
 }
