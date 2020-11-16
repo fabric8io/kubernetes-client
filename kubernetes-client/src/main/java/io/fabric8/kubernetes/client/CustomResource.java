e8deea7c0cfbc2c5fb51da9ce273e3978cd3ce64
Chris Laprun <metacosm@gmail.com>
2020-11-16 12:37:57 +0100

    feat: add isMarkedForDeletion and finalizer-related methods

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/CustomResource.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/CustomResource.java
@@ -70,14 +70,34 @@ public abstract class CustomResource implements HasMetadata {
   public void setApiVersion(String apiVersion) {
     this.apiVersion = apiVersion;
   }
-
+  
   @Override
   public ObjectMeta getMetadata() {
     return metadata;
   }
-
+  
   @Override
   public void setMetadata(ObjectMeta metadata) {
     this.metadata = metadata;
+  }
+  
+  public boolean isMarkedForDeletion() {
+    final String deletionTimestamp = metadata.getDeletionTimestamp();
+    return deletionTimestamp != null && !deletionTimestamp.isEmpty();
+  }
+  
+  public boolean hasFinalizer(String finalizer) {
+    return metadata.getFinalizers().contains(finalizer);
+  }
+  
+  public boolean addFinalizer(String finalizer) {
+    if ((isMarkedForDeletion() || hasFinalizer(finalizer))) {
+      return false;
+    }
+    return metadata.getFinalizers().add(finalizer);
+  }
+  
+  public boolean removeFinalizer(String finalizer) {
+    return metadata.getFinalizers().remove(finalizer);
   }
 }
