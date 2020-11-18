19ee32c9765280fae4203a138c780ecfc861a71d
Chris Laprun <metacosm@gmail.com>
2020-11-18 22:24:14 +0100

    fix: licenses

--- kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/FooList.java
+++ kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/FooList.java
@@ -1,13 +1,12 @@
 /**
- * Copyright 2020 Red Hat, Inc. and/or its affiliates.
+ * Copyright (C) 2015 Red Hat, Inc.
  *
- * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,8 +17,5 @@ package io.fabric8.kubernetes.client.dsl.internal;
 
 import io.fabric8.kubernetes.client.CustomResourceList;
 
-/**
- * @author <a href="claprun@redhat.com">Christophe Laprun</a>
- */
 public class FooList extends CustomResourceList<Foo> {
 }
