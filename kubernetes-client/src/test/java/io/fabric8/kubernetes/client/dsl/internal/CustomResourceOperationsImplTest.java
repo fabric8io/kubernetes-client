2d1a7bf48aea249b36abf7a9ac285d377cfc344e
Chris Laprun <metacosm@gmail.com>
2020-11-19 08:50:59 +0100

    chore: add more tests

--- kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/CustomResourceOperationsImplTest.java
+++ kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/CustomResourceOperationsImplTest.java
@@ -22,6 +22,9 @@ import io.fabric8.kubernetes.api.model.apiextensions.v1beta1.CustomResourceDefin
 import io.fabric8.kubernetes.api.model.apiextensions.v1beta1.CustomResourceDefinitionBuilder;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.CustomResourceList;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
 import io.fabric8.kubernetes.internal.KubernetesDeserializer;
 
@@ -33,6 +36,7 @@ import com.fasterxml.jackson.databind.MappingJsonFactory;
 
 import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class CustomResourceOperationsImplTest {
@@ -42,6 +46,8 @@ public class CustomResourceOperationsImplTest {
 
   public static class MyCustomResourceList extends CustomResourceList<MyCustomResource> {
   }
+  
+  public static class Bar extends CustomResource {}
 
   private final CustomResourceDefinition crd = new CustomResourceDefinitionBuilder()
     .withNewMetadata()
@@ -60,11 +66,25 @@ public class CustomResourceOperationsImplTest {
   .build();
 
   @Test
+  void shouldBeAbleToReturnOperationsWithoutSpecificList() {
+    final MixedOperation<Bar, CustomResourceList, Resource<Bar>> operation = new DefaultKubernetesClient().customResources(Bar.class, CustomResourceList.class);
+    assertNotNull(operation);
+  }
+  
+  @Test
   public void shouldRegisterWithKubernetesDeserializer() throws IOException {
     assertForContext(new CustomResourceOperationContext()
       .withCrd(crd)
       .withType(MyCustomResource.class)
       .withListType(MyCustomResourceList.class));
+  }
+  
+  @Test
+  public void shouldWorkWithPlainCustomResourceList() throws IOException {
+    assertForContext(new CustomResourceOperationContext()
+      .withCrd(crd)
+      .withType(MyCustomResource.class)
+      .withListType(CustomResourceList.class));
   }
 
   @Test
