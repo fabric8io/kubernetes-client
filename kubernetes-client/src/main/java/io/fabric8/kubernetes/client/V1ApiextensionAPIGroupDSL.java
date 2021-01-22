23319fcf0e92777b35f697bb95fec20422a8a45e
Rohan Kumar <rohaan@redhat.com>
2021-01-22 23:36:02 +0530

    Apiextensions DSL uses NonNamespaceOperation for CustomResourceDefinitions
    CustomResourceDefinitions api DSL should not have inNamespace exposed.
    This was due to my mistake while adding apiextensions() DSL in
    https://github.com/fabric8io/kubernetes-client/pull/2359

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/V1ApiextensionAPIGroupDSL.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/V1ApiextensionAPIGroupDSL.java
@@ -17,9 +17,9 @@ package io.fabric8.kubernetes.client;
 
 import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinition;
 import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionList;
-import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
 
 public interface V1ApiextensionAPIGroupDSL extends Client {
-  MixedOperation<CustomResourceDefinition, CustomResourceDefinitionList, Resource<CustomResourceDefinition>> customResourceDefinitions();
+  NonNamespaceOperation<CustomResourceDefinition, CustomResourceDefinitionList, Resource<CustomResourceDefinition>> customResourceDefinitions();
 }
