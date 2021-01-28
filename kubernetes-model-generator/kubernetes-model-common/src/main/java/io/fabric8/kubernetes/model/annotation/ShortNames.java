eb27ad637e50ba8215e9a10d135e8f51e09cb278
Chris Laprun <metacosm@gmail.com>
2021-01-28 22:51:38 +0100

    feat: add ShortNames annotation
    Fixes #2759

---
+++ kubernetes-model-generator/kubernetes-model-common/src/main/java/io/fabric8/kubernetes/model/annotation/ShortNames.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.kubernetes.model.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+
+/**
+ * Specifies which are the short names associated by which instances of the annotated `CustomResource` can be referred to. 
+ */
+@Target({TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ShortNames {
+  /**
+   * The array of possible short names
+   * @return possible short names
+   */
+  String[] value() default {};
+}
