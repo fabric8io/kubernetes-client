08218e959ab8dd9c95731e8699b54e2bf707ffa3
Chris Laprun <metacosm@gmail.com>
2021-01-29 18:01:35 +0100

    fix: import proper version of builder
    That tiny change breaks the generator without any obvious way to figure
    it out unless you step debug the code or know exactly what all the
    decorators expect. :angry:

--- crd-generator/src/main/java/io/dekorate/crd/handler/CustomResourceHandler.java
+++ crd-generator/src/main/java/io/dekorate/crd/handler/CustomResourceHandler.java
@@ -41,7 +41,7 @@ import io.dekorate.crd.visitor.SpecReplicasPathDetector;
 import io.dekorate.crd.visitor.StatusReplicasPathDetector;
 import io.dekorate.kubernetes.decorator.Decorator;
 import io.dekorate.utils.Strings;
-import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionBuilder;
+import io.fabric8.kubernetes.api.model.apiextensions.v1beta1.CustomResourceDefinitionBuilder;
 import io.sundr.codegen.functions.ElementTo;
 import io.sundr.codegen.model.ClassRef;
 import io.sundr.codegen.model.Property;
