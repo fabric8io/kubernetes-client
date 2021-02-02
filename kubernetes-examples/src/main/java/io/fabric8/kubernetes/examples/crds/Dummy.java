2102006f3a1af11675900216d9cc770f232606f4
Rohan Kumar <rohaan@redhat.com>
2021-02-02 18:55:57 +0530

    Chore: Fix CRDExample
    We added support for automatically instantiating Spec and
    Status in https://github.com/fabric8io/kubernetes-client/pull/2739 but
    somehow missed to update our examples

--- kubernetes-examples/src/main/java/io/fabric8/kubernetes/examples/crds/Dummy.java
+++ kubernetes-examples/src/main/java/io/fabric8/kubernetes/examples/crds/Dummy.java
@@ -15,7 +15,6 @@
  */
 package io.fabric8.kubernetes.examples.crds;
 
-import io.fabric8.kubernetes.api.model.KubernetesResource;
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.model.annotation.Group;
@@ -23,7 +22,7 @@ import io.fabric8.kubernetes.model.annotation.Version;
 
 @Version(Dummy.VERSION)
 @Group(Dummy.GROUP)
-public class Dummy extends CustomResource<DummySpec, KubernetesResource> implements Namespaced {
+public class Dummy extends CustomResource<DummySpec, Void> implements Namespaced {
   public static final String GROUP = "demo.fabric8.io";
   public static final String VERSION = "v1";
 }
