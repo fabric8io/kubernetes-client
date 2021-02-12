c94d7cdc1e5e5e5d076ba222f8871294e3704b1e
Chris Laprun <metacosm@gmail.com>
2021-02-12 17:32:43 +0100

    fix: return CustomResourceList if no specific list is found
    Returning KubernetesResourceList results in the list type not being
    registered by KubernetesDeserializer and thus, generic lists not being
    deserialized.

--- kubernetes-client/src/main/java/io/fabric8/kubernetes/client/utils/KubernetesResourceUtil.java
+++ kubernetes-client/src/main/java/io/fabric8/kubernetes/client/utils/KubernetesResourceUtil.java
@@ -24,6 +24,7 @@ import io.fabric8.kubernetes.api.model.KubernetesList;
 import io.fabric8.kubernetes.api.model.KubernetesResourceList;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.api.model.OwnerReference;
+import io.fabric8.kubernetes.client.CustomResourceList;
 import io.fabric8.kubernetes.client.internal.readiness.Readiness;
 import java.time.Duration;
 import java.time.Instant;
@@ -373,7 +374,7 @@ public class KubernetesResourceUtil {
       Class<?> listTypeClass = Thread.currentThread().getContextClassLoader().loadClass(type.getName() + "List");
       return (Class<KubernetesResourceList<T>>) listTypeClass;
     } catch (ClassNotFoundException | ClassCastException e) {
-      return KubernetesResourceList.class;
+      return CustomResourceList.class;
     }
   }
 }
