89eef87bdb11b010be136eb9468b101a0f53dc2f
Chris Laprun <metacosm@gmail.com>
2021-03-11 15:19:06 +0100

    feat: isolate APT-related code, improve CRDGenerator API

--- crd-generator/src/main/java/io/fabric8/crd/generator/CustomResourceInfo.java
+++ crd-generator/src/main/java/io/fabric8/crd/generator/CustomResourceInfo.java
@@ -19,32 +19,17 @@ import io.fabric8.crd.generator.apt.Scope;
 import io.fabric8.kubernetes.api.model.Namespaced;
 import io.fabric8.kubernetes.client.CustomResource;
 import io.fabric8.kubernetes.client.KubernetesClientException;
-import io.fabric8.kubernetes.client.utils.Pluralize;
-import io.fabric8.kubernetes.model.annotation.Group;
-import io.fabric8.kubernetes.model.annotation.Kind;
-import io.fabric8.kubernetes.model.annotation.Plural;
 import io.fabric8.kubernetes.model.annotation.ShortNames;
-import io.fabric8.kubernetes.model.annotation.Singular;
-import io.fabric8.kubernetes.model.annotation.Version;
 import io.sundr.codegen.functions.ClassTo;
-import io.sundr.codegen.functions.ElementTo;
 import io.sundr.codegen.model.TypeDef;
 import io.sundr.codegen.model.TypeRef;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
 import java.util.Optional;
-import javax.lang.model.element.Name;
-import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.DeclaredType;
-import javax.lang.model.type.TypeMirror;
 
 public class CustomResourceInfo {
-  private static final String CUSTOM_RESOURCE_NAME = CustomResource.class.getCanonicalName();
-
   private final String group;
   private final String version;
   private final String kind;
@@ -142,59 +127,6 @@ public class CustomResourceInfo {
 
   public TypeDef definition() {
     return definition;
-  }
-
-  public static CustomResourceInfo fromTypeElement(TypeElement customResource) {
-    final DeclaredType superclass = (DeclaredType) customResource.getSuperclass();
-    final Name crClassName = customResource.getQualifiedName();
-    if (superclass.asElement().toString().equals(CUSTOM_RESOURCE_NAME)) {
-      final List<? extends TypeMirror> typeArguments = superclass.getTypeArguments();
-      if (typeArguments.size() != 2) {
-        System.out.println("Ignoring " + crClassName + " because it isn't parameterized");
-        return null;
-      }
-      final TypeElement spec = ((TypeElement) ((DeclaredType) typeArguments.get(0)).asElement());
-      final TypeElement status = ((TypeElement) ((DeclaredType) typeArguments.get(1)).asElement());
-      Optional<TypeRef> statusType = Optional.empty();
-      if (!status.getQualifiedName().contentEquals(Void.class.getCanonicalName())) {
-        statusType = Optional.of(ElementTo.TYPEDEF.apply(status).toReference());
-      }
-
-      final String group = customResource.getAnnotation(Group.class).value();
-      final String version = customResource.getAnnotation(Version.class).value();
-
-      final String kind = Optional.ofNullable(customResource.getAnnotation(Kind.class))
-        .map(Kind::value)
-        .orElse(customResource.getSimpleName().toString());
-
-      final String singular = Optional.ofNullable(customResource.getAnnotation(Singular.class))
-        .map(Singular::value)
-        .orElse(kind.toLowerCase(Locale.ROOT));
-
-      final String plural = Optional.ofNullable(customResource.getAnnotation(Plural.class))
-        .map(Plural::value)
-        .map(s -> s.toLowerCase(Locale.ROOT))
-        .orElse(Pluralize.toPlural(singular));
-
-      final String[] shortNames = Optional.ofNullable(customResource.getAnnotation(ShortNames.class))
-        .map(ShortNames::value)
-        .orElse(new String[]{});
-
-      final boolean storage = customResource.getAnnotation(Version.class).storage();
-      final boolean served = customResource.getAnnotation(Version.class).served();
-
-      final Scope scope = customResource.getInterfaces().stream()
-        .filter(t -> t.toString().equals(Namespaced.class.getTypeName()))
-        .map(t -> Scope.Namespaced).findFirst().orElse(Scope.Cluster);
-
-      final TypeDef definition = ElementTo.TYPEDEF.apply(customResource);
-
-      return new CustomResourceInfo(group, version, kind, singular, plural, shortNames, storage, served, scope, statusType, definition, crClassName.toString(), spec.getQualifiedName().toString(), status.getQualifiedName().toString());
-    } else {
-      System.out.println(
-        "Ignoring " + crClassName + " because it doesn't extend " + CUSTOM_RESOURCE_NAME);
-      return null;
-    }
   }
 
   private final static String TYPE_NAME = CustomResource.class.getTypeName();
