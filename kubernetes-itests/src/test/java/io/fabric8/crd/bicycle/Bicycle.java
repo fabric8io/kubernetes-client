34ba65d2fc661963456668ee45ff9edb6b062beb
Rohan Kumar <rohaan@redhat.com>
2021-03-12 18:44:27 +0530

    Added Integration test for CustomResource with multiple versions
    + Added IT for a CustomResource with two different versions v1 and
      v1beta1 using the same POJO by providing different
      CustomResourceDefinitionContexts
    
    Related to https://github.com/fabric8io/kubernetes-client/issues/2738 .
    This test demonstrates that it's still supported. However we need to
    discuss this whether this method should be marked as deprecatd or not

---
+++ kubernetes-itests/src/test/java/io/fabric8/crd/bicycle/Bicycle.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.crd.bicycle;
+
+import io.fabric8.kubernetes.client.CustomResource;
+import io.fabric8.kubernetes.model.annotation.Group;
+import io.fabric8.kubernetes.model.annotation.Version;
+
+@Version("v1")
+@Group("demos.fabric8.io")
+public class Bicycle extends CustomResource<BicycleSpec, BicycleStatus> {
+  private String apiVersion;
+
+  @Override
+  public void setApiVersion(String version) {
+    this.apiVersion = version;
+  }
+
+  @Override
+  public String getApiVersion() {
+    return this.apiVersion;
+  }
+}
