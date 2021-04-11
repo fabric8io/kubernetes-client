43145c6e4ed3a32c7d8d1c134396375e3b8dd9df
Chris Laprun <metacosm@gmail.com>
2021-04-12 01:25:26 +0200

    chore(tests): check that unrollHierarchy produces the expected result

--- crd-generator/api/src/test/java/io/fabric8/crd/generator/utils/TypesTest.java
+++ crd-generator/api/src/test/java/io/fabric8/crd/generator/utils/TypesTest.java
@@ -15,14 +15,19 @@
  */
 package io.fabric8.crd.generator.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import io.fabric8.crd.example.basic.Basic;
+import io.fabric8.crd.example.basic.BasicSpec;
+import io.fabric8.crd.example.basic.BasicStatus;
 import io.fabric8.crd.example.webserver.WebServerWithStatusProperty;
 import io.sundr.codegen.functions.ClassTo;
 import io.sundr.codegen.model.ClassRef;
 import io.sundr.codegen.model.Property;
 import io.sundr.codegen.model.TypeDef;
+import io.sundr.codegen.model.TypeParamDef;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -45,6 +50,21 @@ public class TypesTest {
     TypeDef def = ClassTo.TYPEDEF.apply(Basic.class);
     Set<ClassRef> superClasses = Types.getSuperClasses(def);
     assertTrue(superClasses.stream().anyMatch(c -> c.getName().contains("CustomResource")));
+  }
+
+  @Test
+  public void unrollHierarchyShouldProduceProperlyTypedClasses() {
+    TypeDef def = ClassTo.TYPEDEF.apply(Basic.class);
+    Set<TypeDef> defs = Types.unrollHierarchy(def);
+    assertEquals(2, defs.size());
+    Optional<TypeDef> crOpt = defs.stream()
+      .filter(c -> c.getName().contains("CustomResource")).findFirst();
+    assertTrue(crOpt.isPresent());
+    TypeDef crDef = crOpt.get();
+    List<TypeParamDef> parameters = crDef.getParameters();
+    assertEquals(2, parameters.size());
+    assertTrue(parameters.get(0).getName().contains(BasicSpec.class.getSimpleName()));
+    assertTrue(parameters.get(1).getName().contains(BasicStatus.class.getSimpleName()));
   }
 
 }
