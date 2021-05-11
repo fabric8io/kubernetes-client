f82f3f76a60e2c23fce7a8aaef153198c7463a59
Fabian-K <fabiankajzar@googlemail.com>
2021-05-11 17:08:48 +0200

    [tekton-triggers] update to v0.13.0

--- extensions/tekton/generator-triggers/go.mod
+++ extensions/tekton/generator-triggers/go.mod
@@ -2,9 +2,9 @@ module github.com/fabric8io/kubernetes-client/extensions/tekton/generator
 
 require (
 	github.com/fabric8io/kubernetes-client/generator v0.0.0
-	github.com/tektoncd/triggers v0.11.1
-	k8s.io/apimachinery v0.19.0
-	knative.dev/pkg v0.0.0-20210107022335-51c72e24c179
+	github.com/tektoncd/triggers v0.13.0
+	k8s.io/apimachinery v0.19.7
+	knative.dev/pkg v0.0.0-20210130001831-ca02ef752ac6
 	knative.dev/test-infra v0.0.0-20200921012245-37f1a12adbd3 // indirect
 )
 
--- generator/pkg/schemagen/generate.go
+++ generator/pkg/schemagen/generate.go
@@ -218,6 +218,13 @@ func (g *schemaGenerator) javaType(t reflect.Type) string {
 		return g.resolveJavaClassUsingMappingSchema(t)
 	}
 
+	// the interface k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1/JSON is sometimes used to represent any JSON value
+	// example: Tekton Triggers -> Trigger.spec.interceptors.params.value
+	// -> fallback to Object
+	if t.PkgPath() == "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1" && t.Name() == "JSON" {
+		return "java.lang.Object"
+	}
+
 	panic("No type mapping for " + t.PkgPath() + "." + t.Name())
 }
 
