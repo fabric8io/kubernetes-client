2eabc9848197fe7680b00cd0399ed879ff07d7d5
Paul Hemberger <phemberger@hubspot.com>
2021-09-20 09:58:02 -0400

    move tests to kubernetes-tests

--- kubernetes-tests/src/test/java/io/fabric8/kubernetes/client/mock/GenerationTest.java
+++ kubernetes-tests/src/test/java/io/fabric8/kubernetes/client/mock/GenerationTest.java
@@ -13,85 +13,80 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.fabric8.kubernetes;
+package io.fabric8.kubernetes.client.mock;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.arquillian.cube.kubernetes.api.Session;
-import org.arquillian.cube.kubernetes.impl.requirement.RequiresKubernetes;
-import org.arquillian.cube.requirement.ArquillianConditionalRunner;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.ConfigMapBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
+import io.fabric8.kubernetes.client.server.mock.KubernetesMockServer;
 
-@RunWith(ArquillianConditionalRunner.class)
-@RequiresKubernetes
-public class GenerationIT {
-  @ArquillianResource
+@EnableKubernetesMockClient
+class GenerationTest {
+
+  private final static String NAMESPACE = "test-namespace";
+
+  KubernetesMockServer server;
   KubernetesClient client;
-
-  @ArquillianResource
-  Session session;
-
+  
   @Test
   public void testCreatedObjectHasGeneration() {
     ConfigMap configMap = new ConfigMapBuilder()
-      .withNewMetadata().withName(getTestResourcePrefix() + "-configmap").endMetadata()
-      .build();
+        .withNewMetadata()
+        .withName("test-configmap")
+        .withNamespace(NAMESPACE)
+        .endMetadata()
+        .build();
 
     assertNull(configMap.getMetadata().getGeneration());
-    configMap = client.configMaps().inNamespace(session.getNamespace()).createOrReplace(configMap);
+    configMap = client.configMaps().inNamespace(NAMESPACE).createOrReplace(configMap);
     assertEquals(configMap.getMetadata().getGeneration().longValue(), 1L);
   }
 
   @Test
   public void testUpdatedSpecIncrementsGeneration() {
     ConfigMap configMap = new ConfigMapBuilder()
-        .withNewMetadata().withName(getTestResourcePrefix() + "-configmap").endMetadata()
+        .withNewMetadata()
+        .withName("test-configmap")
+        .withNamespace(NAMESPACE)
+        .endMetadata()
         .build();
 
-    configMap = client.configMaps().inNamespace(session.getNamespace()).createOrReplace(configMap);
+    configMap = client.configMaps().inNamespace(NAMESPACE).createOrReplace(configMap);
     assertEquals(configMap.getMetadata().getGeneration().longValue(), 1L);
 
     Map<String, String> configMapData = new HashMap<>();
     configMapData.put("key", "value");
     configMap.setData(configMapData);
 
-    configMap = client.configMaps().inNamespace(session.getNamespace()).createOrReplace(configMap);
+    configMap = client.configMaps().inNamespace(NAMESPACE).createOrReplace(configMap);
     assertEquals(configMap.getMetadata().getGeneration().longValue(), 2L);
   }
 
   @Test
   public void testUpdatingMetadataDoesNotIncrementGeneration() {
     ConfigMap configMap = new ConfigMapBuilder()
-        .withNewMetadata().withName(getTestResourcePrefix() + "-configmap").endMetadata()
+        .withNewMetadata()
+        .withName("test-configmap")
+        .withNamespace(NAMESPACE)
+        .endMetadata()
         .build();
 
-    configMap = client.configMaps().inNamespace(session.getNamespace()).createOrReplace(configMap);
+    configMap = client.configMaps().inNamespace(NAMESPACE).createOrReplace(configMap);
     assertEquals(configMap.getMetadata().getGeneration().longValue(), 1L);
 
     configMap.getMetadata().getAnnotations().put("new-annotation", "new-value");
 
-    configMap = client.configMaps().inNamespace(session.getNamespace()).createOrReplace(configMap);
+    configMap = client.configMaps().inNamespace(NAMESPACE).createOrReplace(configMap);
     assertEquals(configMap.getMetadata().getGeneration().longValue(), 1L);
-  }
-
-  private String getTestResourcePrefix() {
-    return getClass().getSimpleName().toLowerCase();
-  }
-
-  @After
-  public void cleanup() {
-    client.configMaps().inNamespace(session.getNamespace()).withName(getTestResourcePrefix() + "-configmap").delete();
   }
 
 }
