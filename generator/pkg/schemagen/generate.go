a62340c86f0a444f24d3081a51ef8a5afad06f2e
Jose <josecarvajalhilario@gmail.com>
2021-11-18 13:55:10 +0100

    Configure the get field names from the protobuf
    Sometimes the go struct is defined like:
    
    ```go
    type MyClass struct {
       GoField *GoType `protobuf:"...,json=goField,proto3" json:"go_field,omitempty"`
    }
    ```
    
    The json field is wrong ("go_field") and the protobuf field is right "goField". 
    I would say that this is a wrong model, however this is a solution to pick the values from the protobuf section.

--- generator/pkg/schemagen/generate.go
+++ generator/pkg/schemagen/generate.go
@@ -48,6 +48,11 @@ type PackageInformation struct {
 
 // Name strategy mapping for auto discovered classes
 type JavaNameStrategyMapping struct {
+	// Sometimes the json field name is set in this way:
+	// GoField *GoType `protobuf:"...,json=goField,proto3" json:"go_field,omitempty"`
+	// By default, we will still use the one from `json`, if this flag is true,
+	// it will then use the one from the protobuf.
+	ResolveFieldNameFromProtobufFirst bool
 	// To provide a custom generic rule that applies to all java, interface, enum
 	CustomJavaNameRule func(packageName *string, javaName *string)
 	// To manually map a golang class to a Java class
@@ -122,6 +127,14 @@ func GenerateSchemaWithAllOptions(schemaId string, crdLists map[reflect.Type]Crd
 }
 
 func (g *schemaGenerator) jsonFieldName(f reflect.StructField) string {
+	if g.javaNameStrategy.ResolveFieldNameFromProtobufFirst {
+		protobufTag := f.Tag.Get("protobuf")
+		if len(protobufTag) > 0 && strings.Contains(protobufTag, "json=") {
+			fieldName := protobufTag[strings.LastIndex(protobufTag, "json=")+5:]
+			return fieldName[:strings.Index(fieldName, ",")]
+		}
+	}
+
 	json := f.Tag.Get("json")
 	if len(json) > 0 {
 		parts := strings.Split(json, ",")
