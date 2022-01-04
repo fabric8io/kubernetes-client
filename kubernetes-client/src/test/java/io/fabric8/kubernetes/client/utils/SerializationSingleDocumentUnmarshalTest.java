f1f50d5d299ccbba5e4869e6ea26d877cfce815e
Rohan Kumar <rohaan@redhat.com>
2022-01-04 22:33:44 +0530

    Fix #3648: `Serialization.unmarshal` fails to deserialize YAML with single document in presence of document delimiter(`---`)
    Serialization.unmarshal doesn't handle the case when YAML contains only
    single document but yet contains a leading or trailing document
    delimiter(`---`).
    
    Trim YAML document delimiters in case of single documents
    
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

---
+++ kubernetes-client/src/test/java/io/fabric8/kubernetes/client/utils/SerializationSingleDocumentUnmarshalTest.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.kubernetes.client.utils;
+
+import io.fabric8.kubernetes.api.model.KubernetesResource;
+import io.fabric8.kubernetes.api.model.Service;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SerializationSingleDocumentUnmarshalTest {
+  @ParameterizedTest(name = "#{index} - unmarshal {0}")
+  @ValueSource(strings = {"document-with-trailing-document-delimiter.yml", "document-with-leading-document-delimiter.yml", "document-with-leading-and-trailing-document-delimiter.yml", "document-with-no-document-delimiter.yml"})
+  void unmarshalWithSingleDocumentWithDocumentDelimiterShouldReturnKubernetesResource(String arg) {
+    // When
+    final KubernetesResource result = Serialization.unmarshal(
+      SerializationTest.class.getResourceAsStream(String.format("/serialization/%s", arg)),
+      Collections.emptyMap()
+    );
+    // Then
+    assertThat(result)
+      .asInstanceOf(InstanceOfAssertFactories.type(Service.class))
+      .hasFieldOrPropertyWithValue("apiVersion", "v1")
+      .hasFieldOrPropertyWithValue("kind", "Service")
+      .hasFieldOrPropertyWithValue("metadata.name", "redis-master")
+      .hasFieldOrPropertyWithValue("metadata.labels.app", "redis")
+      .hasFieldOrPropertyWithValue("metadata.labels.tier", "backend")
+      .hasFieldOrPropertyWithValue("metadata.labels.role", "master")
+      .hasFieldOrPropertyWithValue("spec.selector.app", "redis")
+      .hasFieldOrPropertyWithValue("spec.selector.tier", "backend")
+      .hasFieldOrPropertyWithValue("spec.selector.role", "master");
+  }
+}
