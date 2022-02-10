b01ad5ac9c4598ef5c3a29226f1cdf7e39605fe6
Marc Nuri <marc@marcnuri.com>
2022-02-10 13:08:58 +0100

    feat(java-gen): Maven Plugin + Integration test infrastructure

---
+++ java-generator/it/src/it/cert-manager/verify.groovy
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import static org.junit.jupiter.api.Assertions.assertEquals
+
+// TODO: This should be modified to test against the current extension sources
+assertEquals(
+  new File(basedir, "/expected/AuthSpec.java").getText("UTF-8").replace("\r\n", "\n"),
+  new File(basedir, "/target/generated-sources/java/io/cert_manager/v1/AuthSpec.java").getText("UTF-8").replace("\r\n", "\n")
+)
+
---
+++ java-generator/maven-plugin/src/main/java/io/fabric8/java/generator/maven/plugin/JavaGeneratorMojo.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.java.generator.maven.plugin;
+
+import io.fabric8.java.generator.CRGeneratorRunner;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.io.File;
+
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class JavaGeneratorMojo extends AbstractMojo {
+
+  @Parameter(defaultValue = "${project}")
+  private MavenProject project;
+
+  // TODO: Modify CRGeneratorRunner to allow passing in a directory too
+  @Parameter(property = "fabric8.java-generator.source", required = true)
+  File crdFile;
+
+  @Parameter(property = "fabric8.java-generator.target", defaultValue = "${basedir}/target/generated-sources/java")
+  File targetDirectory;
+
+  @Override
+  public void execute() {
+    final CRGeneratorRunner runner = new CRGeneratorRunner();
+    runner.run(crdFile, targetDirectory);
+    project.addCompileSourceRoot(targetDirectory.getAbsolutePath());
+  }
+}
