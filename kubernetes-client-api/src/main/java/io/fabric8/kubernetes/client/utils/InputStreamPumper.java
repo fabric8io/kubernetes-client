291de3a9896989c197f4ddce9a72f5a03c8f4e91
wuyanbo <wuyanbo@accesscorporate.com.cn>
2022-03-16 13:58:21 +0800

    feat:format code

--- kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/utils/InputStreamPumper.java
+++ kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/utils/InputStreamPumper.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 Red Hat, Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
 public class InputStreamPumper {
-  
+
   private InputStreamPumper() {
   }
 
@@ -47,7 +47,7 @@ public class InputStreamPumper {
       public int read() throws IOException {
         throw new UnsupportedOperationException();
       }
-      
+
       @Override
       public int read(byte[] b, int off, int len) throws IOException {
         while (!Thread.currentThread().isInterrupted()) {
@@ -103,5 +103,5 @@ public class InputStreamPumper {
       }
     }, executor);
   }
-  
+
 }
