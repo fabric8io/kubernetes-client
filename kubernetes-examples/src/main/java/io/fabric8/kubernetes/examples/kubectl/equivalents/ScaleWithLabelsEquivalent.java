b8931e709a5afc49e39699972d10e2ab49e12e38
Bhavin Gandhi <bhavin7392@gmail.com>
2022-04-07 20:31:30 +0530

    doc: Add kubectl scale with label selector equivalent
    Fixes https://github.com/fabric8io/kubernetes-client/issues/3948
    
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

---
+++ kubernetes-examples/src/main/java/io/fabric8/kubernetes/examples/kubectl/equivalents/ScaleWithLabelsEquivalent.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.kubernetes.examples.kubectl.equivalents;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
+
+/**
+ * Java equivalent of `kubectl scale statefulset --selector=app=my-database --replicas=4`
+ */
+public class ScaleWithLabelsEquivalent {
+  public static void main(String[] args) {
+    try (final KubernetesClient k8s = new KubernetesClientBuilder().build()) {
+      k8s.apps().statefulSets().inNamespace("default")
+          .withLabel("app", "my-database")
+          .resources()
+          .forEach(s -> s.scale(4));
+    }
+  }
+}
