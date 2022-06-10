b7db85e5e8725664d314f50bd081e4c47aea4cf3
skarltjr <kisa0828@naver.com>
2022-06-10 21:55:50 +0900

    chore : do spotless:apply to check code style

--- kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/utils/KubernetesResourceUtil.java
+++ kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/utils/KubernetesResourceUtil.java
@@ -431,7 +431,7 @@ public class KubernetesResourceUtil {
    * @return an object of Secret
    */
   public static Secret createDockerRegistrySecret(String dockerServer, String username, String password, String secretName)
-    throws JsonProcessingException {
+      throws JsonProcessingException {
     Map<String, Object> dockerConfigMap = new HashMap<>();
     Map<String, Object> auths = new HashMap<>();
     Map<String, Object> credentials = new HashMap<>();
@@ -445,9 +445,9 @@ public class KubernetesResourceUtil {
     String dockerConfigAsStr = Serialization.jsonMapper().writeValueAsString(dockerConfigMap);
 
     return new SecretBuilder()
-      .withNewMetadata().withName(secretName).endMetadata()
-      .withType("kubernetes.io/dockerconfigjson")
-      .addToData(".dockerconfigjson", Base64.getEncoder().encodeToString(dockerConfigAsStr.getBytes(StandardCharsets.UTF_8)))
-      .build();
+        .withNewMetadata().withName(secretName).endMetadata()
+        .withType("kubernetes.io/dockerconfigjson")
+        .addToData(".dockerconfigjson", Base64.getEncoder().encodeToString(dockerConfigAsStr.getBytes(StandardCharsets.UTF_8)))
+        .build();
   }
 }
