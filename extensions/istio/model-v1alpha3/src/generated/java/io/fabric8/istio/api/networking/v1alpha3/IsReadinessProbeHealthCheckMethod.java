6687e2e7a216a9ad52f825bc18958229016b17c9
yimcao <yimcao@ebay.com>
2022-08-10 17:31:29 -0700

    add generated files

---
+++ extensions/istio/model-v1alpha3/src/generated/java/io/fabric8/istio/api/networking/v1alpha3/IsReadinessProbeHealthCheckMethod.java
@@ -0,0 +1,25 @@
+
+package io.fabric8.istio.api.networking.v1alpha3;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonTypeResolver;
+import io.fabric8.istio.api.networking.v1alpha3.ReadinessProbeExec;
+import io.fabric8.istio.api.networking.v1alpha3.ReadinessProbeHttpGet;
+import io.fabric8.istio.api.networking.v1alpha3.ReadinessProbeTcpSocket;
+
+@JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeResolver(io.fabric8.kubernetes.model.jackson.UnwrappedTypeResolverBuilder.class)
+@JsonSubTypes({
+    @JsonSubTypes.Type(ReadinessProbeExec.class),
+    @JsonSubTypes.Type(ReadinessProbeHttpGet.class),
+    @JsonSubTypes.Type(ReadinessProbeTcpSocket.class)
+})
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+public interface IsReadinessProbeHealthCheckMethod {
+
+
+}
