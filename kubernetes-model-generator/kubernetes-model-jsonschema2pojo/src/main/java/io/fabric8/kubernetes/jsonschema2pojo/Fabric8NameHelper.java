989d204ce2b2b0dbd7a3c0ade9e999fab7b5f689
Marc Nuri <marc@marcnuri.com>
2022-08-29 12:05:48 +0200

    chore: Fabric8NameHelper renames fields with reserved keywords
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- kubernetes-model-generator/kubernetes-model-jsonschema2pojo/src/main/java/io/fabric8/kubernetes/jsonschema2pojo/Fabric8NameHelper.java
+++ kubernetes-model-generator/kubernetes-model-jsonschema2pojo/src/main/java/io/fabric8/kubernetes/jsonschema2pojo/Fabric8NameHelper.java
@@ -20,22 +20,36 @@ import com.sun.codemodel.JType;
 import org.jsonschema2pojo.GenerationConfig;
 import org.jsonschema2pojo.util.NameHelper;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ * Renames reserved fields name to predefined values (PROTECTED_WORD_MAP)
+ * <p>
  * Overrides default provided NameHelper to overcome getter/setter naming convention mismatch between Sundr.io
  * and jsonschema2pojo.
- *
+ * <p>
  * There are issues with properties such as x-kubernetes-foo or xKubernetesFoo, sundrio expects a getter as
  * getXKubernetesFoo while jsonschema2pojo produces getxKubernetesFoo.
  */
 public class Fabric8NameHelper extends NameHelper {
 
+  private static final Map<String, String> PROTECTED_WORD_MAP = new HashMap<>();
+  static {
+    PROTECTED_WORD_MAP.put("class", "className");
+  }
   private static final Pattern SINGLE_LETTER_PREFIX_WORD_PROPERTY = Pattern.compile("^[a-z]((-[a-zA-Z])|[A-Z])(.*)$");
 
   public Fabric8NameHelper(GenerationConfig generationConfig) {
     super(generationConfig);
+  }
+
+  @Override
+  public String getFieldName(String propertyName, JsonNode node) {
+    final String fieldName = super.getFieldName(propertyName, node);
+    return PROTECTED_WORD_MAP.getOrDefault(fieldName, fieldName);
   }
 
   @Override
