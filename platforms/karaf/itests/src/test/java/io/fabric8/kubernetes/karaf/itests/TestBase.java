5a993d3baffe80733f1f62a271a1386ea5e113be
Marc Nuri <marc@marcnuri.com>
2022-08-30 16:17:39 +0200

    test: refactor karaf deserialization tests to ensure GenericKuberentesResource errors
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- platforms/karaf/itests/src/test/java/io/fabric8/kubernetes/karaf/itests/TestBase.java
+++ platforms/karaf/itests/src/test/java/io/fabric8/kubernetes/karaf/itests/TestBase.java
@@ -45,62 +45,61 @@ public class TestBase {
   }
 
   Option[] baseConfiguration(Option features, List<Option> extraOptions) {
-    MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf-minimal").versionAsInProject().type("tar.gz");
+    MavenArtifactUrlReference karafUrl = maven().groupId("org.apache.karaf").artifactId("apache-karaf-minimal")
+        .versionAsInProject().type("tar.gz");
     final List<Option> ret = new ArrayList<>();
     if (JavaVersionUtil.getMajorVersion() >= 9) {
       ret.addAll(Arrays.asList(
-        karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
-        configureSecurity().disableKarafMBeanServerBuilder(),
-        features,
-        editConfigurationFileExtend(
-          "etc/io.fabric8.kubernetes.client.cfg", "junit", "ignored"),
-        editConfigurationFileExtend(
-          "etc/org.ops4j.pax.url.mvn.cfg",
-          "org.ops4j.pax.url.mvn.repositories",
-          "https://repo1.maven.org/maven2/"),
-        keepRuntimeFolder(),
-        logLevel(LogLevelOption.LogLevel.INFO),
-        new VMOption("--add-exports=java.base/"
-          + "org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
-        new VMOption("--patch-module"),
-        new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-"
-          + System.getProperty("karaf.version", "4.3.5") + ".jar"),
-        new VMOption("--patch-module"),
-        new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-"
-          + System.getProperty("karaf.version", "4.3.5") + ".jar"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.security=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.net=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.lang=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.base/java.util=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.naming/javax.naming.spi=ALL-UNNAMED"),
-        new VMOption("--add-opens"),
-        new VMOption("java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED"),
-        new VMOption("--add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED"),
-        new VMOption("--add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED"),
-        new VMOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
-        new VMOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
-        new VMOption("-classpath"),
-        new VMOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*")
-      ));
+          karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
+          configureSecurity().disableKarafMBeanServerBuilder(),
+          features,
+          editConfigurationFileExtend(
+              "etc/io.fabric8.kubernetes.client.cfg", "junit", "ignored"),
+          editConfigurationFileExtend(
+              "etc/org.ops4j.pax.url.mvn.cfg",
+              "org.ops4j.pax.url.mvn.repositories",
+              "https://repo1.maven.org/maven2/"),
+          keepRuntimeFolder(),
+          logLevel(LogLevelOption.LogLevel.INFO),
+          new VMOption("--add-exports=java.base/"
+              + "org.apache.karaf.specs.locator=java.xml,ALL-UNNAMED"),
+          new VMOption("--patch-module"),
+          new VMOption("java.base=lib/endorsed/org.apache.karaf.specs.locator-"
+              + System.getProperty("karaf.version", "4.4.1") + ".jar"),
+          new VMOption("--patch-module"),
+          new VMOption("java.xml=lib/endorsed/org.apache.karaf.specs.java.xml-"
+              + System.getProperty("karaf.version", "4.4.1") + ".jar"),
+          new VMOption("--add-opens"),
+          new VMOption("java.base/java.security=ALL-UNNAMED"),
+          new VMOption("--add-opens"),
+          new VMOption("java.base/java.net=ALL-UNNAMED"),
+          new VMOption("--add-opens"),
+          new VMOption("java.base/java.lang=ALL-UNNAMED"),
+          new VMOption("--add-opens"),
+          new VMOption("java.base/java.util=ALL-UNNAMED"),
+          new VMOption("--add-opens"),
+          new VMOption("java.naming/javax.naming.spi=ALL-UNNAMED"),
+          new VMOption("--add-opens"),
+          new VMOption("java.rmi/sun.rmi.transport.tcp=ALL-UNNAMED"),
+          new VMOption("--add-exports=java.base/sun.net.www.protocol.http=ALL-UNNAMED"),
+          new VMOption("--add-exports=java.base/sun.net.www.protocol.https=ALL-UNNAMED"),
+          new VMOption("--add-exports=java.base/sun.net.www.protocol.jar=ALL-UNNAMED"),
+          new VMOption("--add-exports=jdk.naming.rmi/com.sun.jndi.url.rmi=ALL-UNNAMED"),
+          new VMOption("-classpath"),
+          new VMOption("lib/jdk9plus/*" + File.pathSeparator + "lib/boot/*")));
     } else {
       ret.addAll(Arrays.asList(
-        karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
-        configureSecurity().disableKarafMBeanServerBuilder(),
-        features,
-        editConfigurationFileExtend(
-          "etc/io.fabric8.kubernetes.client.cfg", "junit", "ignored"),
-        editConfigurationFileExtend(
-          "etc/org.ops4j.pax.url.mvn.cfg",
-          "org.ops4j.pax.url.mvn.repositories",
-          "https://repo1.maven.org/maven2/"),
-        keepRuntimeFolder(),
-        logLevel(LogLevelOption.LogLevel.INFO)
-      ));
+          karafDistributionConfiguration().frameworkUrl(karafUrl).name("Apache Karaf").unpackDirectory(new File("target/exam")),
+          configureSecurity().disableKarafMBeanServerBuilder(),
+          features,
+          editConfigurationFileExtend(
+              "etc/io.fabric8.kubernetes.client.cfg", "junit", "ignored"),
+          editConfigurationFileExtend(
+              "etc/org.ops4j.pax.url.mvn.cfg",
+              "org.ops4j.pax.url.mvn.repositories",
+              "https://repo1.maven.org/maven2/"),
+          keepRuntimeFolder(),
+          logLevel(LogLevelOption.LogLevel.INFO)));
     }
     ret.addAll(extraOptions);
     return ret.toArray(new Option[0]);
