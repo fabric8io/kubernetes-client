8a81da4f2f45ffcfeb996bbb986ae15c6baaa6b9
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-09-19 10:41:58 +0100

    [java-gen] Preserve unknown without type should translate to JsonNode

--- java-generator/core/src/main/java/io/fabric8/java/generator/nodes/JPrimitiveNameAndType.java
+++ java-generator/core/src/main/java/io/fabric8/java/generator/nodes/JPrimitiveNameAndType.java
@@ -23,6 +23,7 @@ public class JPrimitiveNameAndType extends JavaNameAndType {
   static final JPrimitiveNameAndType FLOAT = new JPrimitiveNameAndType("Float");
   static final JPrimitiveNameAndType DOUBLE = new JPrimitiveNameAndType("Double");
   static final JPrimitiveNameAndType STRING = new JPrimitiveNameAndType("String");
+  static final JPrimitiveNameAndType JSON_NODE = new JPrimitiveNameAndType("com.fasterxml.jackson.databind.JsonNode");
 
   public JPrimitiveNameAndType(String name) {
     super(name, JavaType.PRIMITIVE);
