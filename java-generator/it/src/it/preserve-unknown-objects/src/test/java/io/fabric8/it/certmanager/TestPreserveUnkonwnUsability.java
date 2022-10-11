b36feadfc8f8135570a890216a185c912ef9be07
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-10-11 17:42:12 +0100

    fix the integration test

--- java-generator/it/src/it/preserve-unknown-objects/src/test/java/io/fabric8/it/certmanager/TestPreserveUnkonwnUsability.java
+++ java-generator/it/src/it/preserve-unknown-objects/src/test/java/io/fabric8/it/certmanager/TestPreserveUnkonwnUsability.java
@@ -16,11 +16,10 @@
 package io.fabric8.it.certmanager;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.IntNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 import io.dapr.v1alpha1.Component;
 import io.dapr.v1alpha1.ComponentSpec;
 import io.dapr.v1alpha1.componentspec.Metadata;
+import io.fabric8.kubernetes.api.model.AnyType;
 import io.fabric8.kubernetes.api.model.ObjectMeta;
 import io.fabric8.kubernetes.client.utils.Serialization;
 import org.junit.jupiter.api.Test;
@@ -30,6 +29,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -57,23 +58,26 @@ class TestPreserveUnknownUsability {
 
     Metadata meta1 = new Metadata();
     meta1.setName("consumerID");
-    meta1.setValue(new TextNode("{uuid}"));
+    meta1.setValue(new AnyType("{uuid}"));
 
     Metadata meta2 = new Metadata();
     meta2.setName("url");
-    meta2.setValue(new TextNode("tcp://admin:public@localhost:1883"));
+    meta2.setValue(new AnyType("tcp://admin:public@localhost:1883"));
 
     Metadata meta3 = new Metadata();
     meta3.setName("qos");
-    meta3.setValue(new IntNode(1));
+    meta3.setValue(new AnyType(1));
 
     Metadata meta4 = new Metadata();
     meta4.setName("anArray");
-    meta4.setValue(Serialization.jsonMapper().readTree("[1,2,3]"));
+    meta4.setValue(new AnyType(new int[]{1, 2, 3}));
 
     Metadata meta5 = new Metadata();
     meta5.setName("anObject");
-    meta5.setValue(Serialization.jsonMapper().readTree("{ \"ONE\": 1, \"TWO\": 2 }"));
+    Map<String, Object> obj = new HashMap();
+    obj.put("ONE", 1);
+    obj.put("TWO", 2);
+    meta5.setValue(new AnyType(obj));
 
     List<Metadata> lst = new ArrayList();
     lst.add(meta1);
