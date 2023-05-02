ee38b09e4aa53d841d0d209cda39c4c6fa316afc
Steve Hawkins <shawkins@redhat.com>
2023-05-02 07:48:22 -0400

    fix #5091 reverting to byte[] simple requests

--- kubernetes-client/src/test/java/io/fabric8/kubernetes/client/impl/DryRunTest.java
+++ kubernetes-client/src/test/java/io/fabric8/kubernetes/client/impl/DryRunTest.java
@@ -35,7 +35,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -59,7 +58,7 @@ class DryRunTest {
     builders = new ArrayList<>();
     this.mockClient = Mockito.mock(HttpClient.class, Mockito.RETURNS_DEEP_STUBS);
     Config config = new ConfigBuilder().withMasterUrl("https://localhost:8443/").build();
-    when(mockClient.sendAsync(any(), Mockito.eq(InputStream.class)))
+    when(mockClient.sendAsync(any(), Mockito.eq(byte[].class)))
         .thenReturn(CompletableFuture.completedFuture(TestHttpResponse.from(200,
             "{\"kind\":\"Pod\", \"apiVersion\":\"v1\"}")));
     kubernetesClient = new KubernetesClientImpl(mockClient, config);
