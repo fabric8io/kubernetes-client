4b4a47d499fa5bbe57415f3d915fab9b33e32c18
Marc Nuri <marc@marcnuri.com>
2023-06-16 17:59:40 +0200

    test:refactor: use TestStandardHttpClientFactory
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/core/v1/ServiceAccountOperationsImplTest.java
+++ kubernetes-client/src/test/java/io/fabric8/kubernetes/client/dsl/internal/core/v1/ServiceAccountOperationsImplTest.java
@@ -15,125 +15,95 @@
  */
 package io.fabric8.kubernetes.client.dsl.internal.core.v1;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.fabric8.kubernetes.api.model.authentication.TokenRequest;
 import io.fabric8.kubernetes.api.model.authentication.TokenRequestBuilder;
-import io.fabric8.kubernetes.client.Client;
-import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.fabric8.kubernetes.client.dsl.ServiceAccountResource;
-import io.fabric8.kubernetes.client.http.HttpClient;
-import io.fabric8.kubernetes.client.http.HttpRequest;
+import io.fabric8.kubernetes.client.http.AsyncBody;
+import io.fabric8.kubernetes.client.http.TestAsyncBody;
 import io.fabric8.kubernetes.client.http.TestHttpResponse;
-import io.fabric8.kubernetes.client.impl.BaseClient;
-import io.fabric8.kubernetes.client.utils.KubernetesSerialization;
+import io.fabric8.kubernetes.client.http.TestStandardHttpClient;
+import io.fabric8.kubernetes.client.http.TestStandardHttpClientFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
-import static java.net.HttpURLConnection.HTTP_CREATED;
-import static java.net.HttpURLConnection.HTTP_NOT_FOUND;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 class ServiceAccountOperationsImplTest {
-  private ServiceAccountOperationsImpl serviceAccountOperations;
-  private HttpClient mockedHttpClient;
+
+  private TestStandardHttpClientFactory factory;
+  private KubernetesClient client;
 
   @BeforeEach
   void setUp() {
-    Client mockedClient = mock(Client.class);
-    BaseClient mockedBaseClient = mock(BaseClient.class);
-    mockedHttpClient = mock(HttpClient.class);
-    when(mockedClient.getConfiguration()).thenReturn(new ConfigBuilder()
-        .withMasterUrl("http://test.kubernetes.server")
-        .build());
-    when(mockedClient.getHttpClient()).thenReturn(mockedHttpClient);
-    when(mockedClient.adapt(BaseClient.class)).thenReturn(mockedBaseClient);
-    when(mockedBaseClient.getKubernetesSerialization()).thenReturn(new KubernetesSerialization(new ObjectMapper(), false));
-    serviceAccountOperations = new ServiceAccountOperationsImpl(mockedClient);
+    factory = new TestStandardHttpClientFactory();
+    client = new KubernetesClientBuilder().withConfig(Config.empty()).withHttpClientFactory(factory).build();
   }
 
   @Test
   void tokenRequest_whenApiServerCallSuccessful_thenReturnTokenInTokenRequestStatus() throws URISyntaxException {
     // Given
-    String responseFromServer = "{\"kind\":\"TokenRequest\",\"apiVersion\":\"authentication.k8s.io/v1\"," +
+    final String responseFromServer = "{\"kind\":\"TokenRequest\",\"apiVersion\":\"authentication.k8s.io/v1\"," +
         "\"status\":{\"token\":\"my.secret.token\",\"expirationTimestamp\":\"2023-06-14T18:42:15Z\"}}";
-    HttpRequest.Builder mockHttpRequestBuilder = createMockHttpRequestBuilder();
-    HttpRequest mockHttpRequest = createMockHttpRequest(mockHttpRequestBuilder);
-    when(mockedHttpClient.sendAsync(mockHttpRequest, byte[].class))
-        .thenReturn(CompletableFuture.completedFuture(new TestHttpResponse<byte[]>().withCode(HTTP_CREATED)
-            .withBody(responseFromServer.getBytes(StandardCharsets.UTF_8))));
+    factory.getInstances().iterator().next()
+        .expect("/api/v1/namespaces/test/serviceaccounts/my-serviceaccount/token", (r, c) -> {
+          final AsyncBody body = new TestAsyncBody();
+          c.consume(Collections.singletonList(ByteBuffer.wrap((responseFromServer).getBytes(StandardCharsets.UTF_8))), body);
+          return CompletableFuture.completedFuture(new TestHttpResponse<AsyncBody>().withCode(200).withBody(body));
+        });
     TokenRequest tokenRequest = new TokenRequestBuilder()
         .withNewSpec()
         .withAudiences("http://example.svc")
         .endSpec()
         .build();
-
     // When
-    TokenRequest updatedTokenRequest = serviceAccountOperations.inNamespace("test").withName("my-serviceaccount")
+    TokenRequest updatedTokenRequest = client.serviceAccounts().inNamespace("test").withName("my-serviceaccount")
         .tokenRequest(tokenRequest);
-
     // Then
-    verify(mockHttpRequestBuilder, times(1)).post(eq("application/json"), anyString());
-    ArgumentCaptor<URL> urlArgumentCaptor = ArgumentCaptor.forClass(URL.class);
-    verify(mockHttpRequestBuilder, times(1)).url(urlArgumentCaptor.capture());
-    assertThat(urlArgumentCaptor.getValue()).hasPath("/api/v1/namespaces/test/serviceaccounts/my-serviceaccount/token");
+    Assertions.assertThat(factory.getInstances())
+        .singleElement()
+        .extracting(TestStandardHttpClient::getRecordedConsumeBytesDirects)
+        .asList()
+        .hasSize(1)
+        .singleElement()
+        .extracting("request")
+        .extracting("uri").asString()
+        .endsWith("/api/v1/namespaces/test/serviceaccounts/my-serviceaccount/token");
     assertThat(updatedTokenRequest)
         .hasFieldOrPropertyWithValue("status.token", "my.secret.token")
         .hasFieldOrPropertyWithValue("status.expirationTimestamp", "2023-06-14T18:42:15Z");
   }
 
   @Test
-  void tokenRequest_whenApiServerRequestFailure_thenThrowException() throws URISyntaxException {
-    String responseFromServer = "{\"kind\":\"Status\",\"apiVersion\":\"v1\",\"status\":\"Failure\",\"message\":\"serviceaccount my-serviceaccount not found\",\"reason\":\"NotFound\",\"details\":{\"name\":\"my-service-account\",\"kind\":\"serviceaccounts\"},\"code\":404}";
-    HttpRequest.Builder mockHttpRequestBuilder = createMockHttpRequestBuilder();
-    HttpRequest mockHttpRequest = createMockHttpRequest(mockHttpRequestBuilder);
-    when(mockedHttpClient.sendAsync(mockHttpRequest, byte[].class))
-        .thenReturn(CompletableFuture.completedFuture(new TestHttpResponse<byte[]>().withCode(HTTP_NOT_FOUND)
-            .withBody(responseFromServer.getBytes(StandardCharsets.UTF_8))));
-    ServiceAccountResource serviceAccountResource = serviceAccountOperations.inNamespace("test").withName("my-serviceaccount");
-
-    // When
+  void tokenRequest_whenApiServerRequestFailure_thenThrowException() {
+    // Given
+    final String responseFromServer = "{\"kind\":\"Status\",\"apiVersion\":\"v1\",\"status\":\"Failure\",\"message\":\"serviceaccount my-serviceaccount not found\",\"reason\":\"NotFound\",\"details\":{\"name\":\"my-service-account\",\"kind\":\"serviceaccounts\"},\"code\":404}";
+    factory.getInstances().iterator().next()
+        .expect("/api/v1/namespaces/test/serviceaccounts/my-serviceaccount/token", (r, c) -> {
+          final AsyncBody body = new TestAsyncBody();
+          c.consume(Collections.singletonList(ByteBuffer.wrap((responseFromServer).getBytes(StandardCharsets.UTF_8))), body);
+          return CompletableFuture.completedFuture(new TestHttpResponse<AsyncBody>().withCode(404).withBody(body));
+        });
+    ServiceAccountResource serviceAccountResource = client.serviceAccounts()
+        .inNamespace("test").withName("my-serviceaccount");
+    // When - Then
     assertThatExceptionOfType(KubernetesClientException.class)
         .isThrownBy(serviceAccountResource::tokenRequest)
         .withMessageContaining("serviceaccount my-serviceaccount not found")
-        .hasFieldOrPropertyWithValue("code", HTTP_NOT_FOUND)
-        .hasFieldOrPropertyWithValue("status.code", HTTP_NOT_FOUND)
+        .hasFieldOrPropertyWithValue("code", 404)
+        .hasFieldOrPropertyWithValue("status.code", 404)
         .hasFieldOrPropertyWithValue("status.message", "serviceaccount my-serviceaccount not found")
         .hasFieldOrPropertyWithValue("status.reason", "NotFound");
-  }
-
-  private HttpRequest createMockHttpRequest(HttpRequest.Builder mockHttpRequestBuilder) throws URISyntaxException {
-    HttpRequest mockHttpRequest = mock(HttpRequest.class);
-    when(mockHttpRequestBuilder.build()).thenReturn(mockHttpRequest);
-    when(mockHttpRequest.uri())
-        .thenReturn(new URI("http://test.kubernetes.server/api/v1/namespaces/test/serviceaccounts/my-serviceaccount/token"));
-    when(mockHttpRequest.method()).thenReturn("POST");
-    return mockHttpRequest;
-  }
-
-  private HttpRequest.Builder createMockHttpRequestBuilder() {
-    HttpRequest.Builder mockHttpRequestBuilder = mock(HttpRequest.Builder.class);
-    when(mockedHttpClient.newHttpRequestBuilder()).thenReturn(mockHttpRequestBuilder);
-    when(mockHttpRequestBuilder.post(anyString(), anyString())).thenReturn(mockHttpRequestBuilder);
-    when(mockHttpRequestBuilder.url(any(URL.class))).thenReturn(mockHttpRequestBuilder);
-    when(mockHttpRequestBuilder.timeout(anyLong(), any(TimeUnit.class))).thenReturn(mockHttpRequestBuilder);
-    return mockHttpRequestBuilder;
   }
 }
