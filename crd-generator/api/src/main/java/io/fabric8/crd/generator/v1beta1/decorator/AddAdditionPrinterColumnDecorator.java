986c525b268b1cd49b4fa471bc1d7b38d777cf98
bachmanity1 <bachmanity138@gmail.com>
2023-07-19 18:07:05 +0600

    fix code style & update docs

--- crd-generator/api/src/main/java/io/fabric8/crd/generator/v1beta1/decorator/AddAdditionPrinterColumnDecorator.java
+++ crd-generator/api/src/main/java/io/fabric8/crd/generator/v1beta1/decorator/AddAdditionPrinterColumnDecorator.java
@@ -122,6 +122,7 @@ public class AddAdditionPrinterColumnDecorator extends
 
   @Override
   public String toString() {
-    return getClass().getName() + " [name:" + getName() + ", version:" + getVersion() + "column:" + columnName + "priority:" + priority + "]";
+    return getClass().getName() + " [name:" + getName() + ", version:" + getVersion() + "column:" + columnName + "priority:"
+        + priority + "]";
   }
 }
