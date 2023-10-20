ff736596a9e9104ec93c3f90b02aafe572e349fa
Marc Nuri <marc@marcnuri.com>
2023-10-20 09:26:21 +0200

    test: TypeInference tests multiple scenarios
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/TypeInferenceTest.java
+++ kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/TypeInferenceTest.java
@@ -1,9 +1,25 @@
+/**
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.fabric8.kubernetes.client;
 
 import io.fabric8.kubernetes.api.model.ConfigMap;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.Service;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -11,11 +27,22 @@ import java.util.Optional;
 
 class TypeInferenceTest {
 
+  // Commented-out due to compilation failure
+  // TODO: Enable after #5522 is fixed
+  //  @Test
+  //  void typeInferenceWithLambda() {
+  //    final ConfigMapDependentResource cdr = new ConfigMapDependentResource();
+  //    final DeploymentDependentResource ddr = new DeploymentDependentResource();
+  //    Arrays.asList(cdr, ddr).forEach(dr -> dr.reconcile());
+  //    Assertions.assertTrue(true);
+  //  }
+
   @Test
-  void test() {
+  void typeInferenceWithMethodReference() {
     final ConfigMapDependentResource cdr = new ConfigMapDependentResource();
     final DeploymentDependentResource ddr = new DeploymentDependentResource();
-    Arrays.asList(cdr, ddr).forEach(dr -> dr.reconcile());
+    Arrays.asList(cdr, ddr).forEach(KubernetesDependentResource::reconcile);
+    Assertions.assertTrue(true);
   }
 
   static abstract class KubernetesDependentResource<R extends HasMetadata, P extends HasMetadata>
