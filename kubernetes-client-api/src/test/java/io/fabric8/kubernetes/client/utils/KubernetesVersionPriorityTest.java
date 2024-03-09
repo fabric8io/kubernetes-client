a1e854b542e6ad711831af5d1d5b0f9e3226578c
Bernhard Strähle <bernhard.straehle@gmail.com>
2024-03-09 22:36:30 +0100

    Add sortByPriority for generic lists to KubernetesVersionPriority utility class

--- kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/utils/KubernetesVersionPriorityTest.java
+++ kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/utils/KubernetesVersionPriorityTest.java
@@ -15,6 +15,8 @@
  */
 package io.fabric8.kubernetes.client.utils;
 
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionVersion;
+import io.fabric8.kubernetes.api.model.apiextensions.v1.CustomResourceDefinitionVersionBuilder;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -43,5 +45,57 @@ class KubernetesVersionPriorityTest {
     String computed = KubernetesVersionPriority.highestPriority(versions);
     // then
     assertThat(computed).isEqualTo(highest);
+  }
+
+  @Test
+  void should_sort_with_highest_priority() {
+    // given
+    CustomResourceDefinitionVersion foo10 = createCustomResourceDefinitionVersion("foo10");
+    CustomResourceDefinitionVersion v11alpha2 = createCustomResourceDefinitionVersion("v11alpha2");
+    CustomResourceDefinitionVersion foo1 = createCustomResourceDefinitionVersion("foo1");
+    CustomResourceDefinitionVersion v3beta1 = createCustomResourceDefinitionVersion("v3beta1");
+    CustomResourceDefinitionVersion v2 = createCustomResourceDefinitionVersion("v2");
+    CustomResourceDefinitionVersion v10beta3 = createCustomResourceDefinitionVersion("v10beta3");
+    CustomResourceDefinitionVersion v10 = createCustomResourceDefinitionVersion("v10");
+    CustomResourceDefinitionVersion v11beta2 = createCustomResourceDefinitionVersion("v11beta2");
+    CustomResourceDefinitionVersion v1 = createCustomResourceDefinitionVersion("v1");
+    CustomResourceDefinitionVersion v12alpha1 = createCustomResourceDefinitionVersion("v12alpha1");
+
+    List<CustomResourceDefinitionVersion> versions = Arrays.asList(
+        foo10,
+        v11alpha2,
+        foo1,
+        v3beta1,
+        v2,
+        v10beta3,
+        v10,
+        v11beta2,
+        v1,
+        v12alpha1);
+    // when
+    List<CustomResourceDefinitionVersion> computed = KubernetesVersionPriority
+        .sortByPriority(versions, CustomResourceDefinitionVersion::getName);
+    // then
+    List<CustomResourceDefinitionVersion> expected = Arrays.asList(
+        v10,
+        v2,
+        v1,
+        v11beta2,
+        v10beta3,
+        v3beta1,
+        v12alpha1,
+        v11alpha2,
+        foo1,
+        foo10);
+    // ensure same size and ordering
+    assertThat(computed).isEqualTo(expected);
+    // ensure that the implementation works only on references
+    assertThat(computed.get(0)).isSameAs(v10);
+  }
+
+  private static CustomResourceDefinitionVersion createCustomResourceDefinitionVersion(String name) {
+    return new CustomResourceDefinitionVersionBuilder()
+        .withName(name)
+        .build();
   }
 }
