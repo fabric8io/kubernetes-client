19974ffed0d3cededa65c5e92fb5bd4142b6dab5
Piotr P. Karwasz <piotr.github@karwasz.org>
2024-03-31 22:59:30 +0200

    Remove `public` modifier from JUnit 5 test

--- log4j/src/test/java/io/fabric8/kubernetes/log4j/lookup/it/KubernetesLookupIT.java
+++ log4j/src/test/java/io/fabric8/kubernetes/log4j/lookup/it/KubernetesLookupIT.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class KubernetesLookupIT {
+class KubernetesLookupIT {
 
   @Test
   void should_find_lookup() {
