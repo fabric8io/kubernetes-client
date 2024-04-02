4fbaa128a60b130b61c8ac81ec893f84a80438cf
Ikhun Um <ih.pert@gmail.com>
2024-04-02 13:09:29 +0900

    Drain `HttpResponse` in tests
    Motivation:
    
    In Armeria CI, we saw Netty leak reports from kubernetes-client CI tests.
    After investigating, I found out that some of the tests did not
    consume `HttpResponse`. As a result, some `ByteBuf`s were not
    subscribed to and remained in the Publisher's internal buffer.
    
    Modifications:
    
    - Fixed to call `AsyncBody.consume()` where it was not called before.
    
    Result:
    
    No more leaks in CI builds.

--- kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/http/AbstractSimultaneousConnectionsTest.java
+++ kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/http/AbstractSimultaneousConnectionsTest.java
@@ -116,9 +116,13 @@ public abstract class AbstractSimultaneousConnectionsTest {
       }
       CompletableFuture.allOf(asyncResponses.toArray(new CompletableFuture[0])).get(60, TimeUnit.SECONDS);
       assertThat(asyncResponses)
-          .hasSize(MAX_HTTP_1_CONNECTIONS)
-          .extracting(CompletableFuture::join)
-          .extracting(HttpResponse::code).containsOnly(204);
+        .hasSize(MAX_HTTP_1_CONNECTIONS)
+        .extracting(CompletableFuture::join)
+        .extracting(response -> {
+          response.body().consume();
+          return response.code();
+        })
+        .containsOnly(204);
     }
   }
 
