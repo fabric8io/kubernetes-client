e1c310354986f2b0ebccbcfbbea8706decd35346
Bernhard Strähle <bernhard.straehle@gmail.com>
2024-04-11 00:37:13 +0200

    Fix code smell in MultipleVersionsCRDTest

--- crd-generator/test/src/test/java/io/fabric8/crd/generator/multiple/MultipleVersionsCRDTest.java
+++ crd-generator/test/src/test/java/io/fabric8/crd/generator/multiple/MultipleVersionsCRDTest.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class MultipleVersionsCRDTest {
+class MultipleVersionsCRDTest {
 
   @Test
   void testCrd() {
