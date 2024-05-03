1870f8ad09188fbc19c90ef8fbbfd1226cbdaaca
Bernhard Strähle <bernhard.straehle@gmail.com>
2024-05-03 11:56:28 +0200

    Refactor crd-generator-maven-plugin to use collector module

---
+++ crd-generator/maven-plugin/src/main/java/io/fabric8/crd/generator/maven/plugin/FilterSet.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2015 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.fabric8.crd.generator.maven.plugin;
+
+import java.util.List;
+
+public class FilterSet {
+  private List<String> packages;
+  private List<String> versions;
+  private List<String> groups;
+
+  public List<String> getPackages() {
+    return packages;
+  }
+
+  public void setPackages(List<String> packages) {
+    this.packages = packages;
+  }
+
+  public List<String> getVersions() {
+    return versions;
+  }
+
+  public void setVersions(List<String> versions) {
+    this.versions = versions;
+  }
+
+  public List<String> getGroups() {
+    return groups;
+  }
+
+  public void setGroups(List<String> groups) {
+    this.groups = groups;
+  }
+}
