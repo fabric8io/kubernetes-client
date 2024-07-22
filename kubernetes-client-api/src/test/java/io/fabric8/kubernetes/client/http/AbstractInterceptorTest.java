f854fc954f7660e842a82b13dea321177ad8ec91
Sam Barker <sbarker@redhat.com>
2024-07-22 17:04:49 +1200

    Add test which covers future being completed exceptionally with a CompletionException.
    This happens in the Jetty implementation but  gets wrapped in an IOException by the OkHttp impl but this should be enough for the coverage checker.

--- kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/http/AbstractInterceptorTest.java
+++ kubernetes-client-api/src/test/java/io/fabric8/kubernetes/client/http/AbstractInterceptorTest.java
@@ -22,6 +22,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -31,6 +32,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -230,6 +232,37 @@ public abstract class AbstractInterceptorTest {
       // Then
       assertThat(response).succeedsWithin(Duration.of(30, ChronoUnit.SECONDS));
       assertThat(afterInvoked).extracting(CountDownLatch::getCount).isEqualTo(0L);
+    }
+  }
+
+  @Test
+  @DisplayName("afterConnectionFailure, completionExceptions are passed through")
+  public void afterConnectionFailureCompletionException() {
+    // Given
+    final CountDownLatch connectionFailureCallbackInvoked = new CountDownLatch(1);
+    final HttpClient.Builder builder = getHttpClientFactory().newBuilder()
+        .connectTimeout(1, TimeUnit.SECONDS)
+        .addOrReplaceInterceptor("test", new Interceptor() {
+          @Override
+          public void afterConnectionFailure(HttpRequest request, Throwable failure) {
+            connectionFailureCallbackInvoked.countDown();
+          }
+        });
+    // When
+    try (HttpClient client = builder.build()) {
+      final CompletableFuture<HttpResponse<String>> response = client.sendAsync(client.newHttpRequestBuilder()
+          .timeout(1, TimeUnit.SECONDS)
+          .uri(server.url("/intercepted-url"))
+          .method("POST", "application/json", new InputStream() {
+            @Override
+            public int read() {
+              throw new CompletionException("boom time", null); // gets propagated by jetty but gets wrapped by OkHttp
+            }
+          }, 1L).build(), String.class);
+
+      // Then
+      assertThat(response).failsWithin(Duration.of(30, ChronoUnit.SECONDS));
+      assertThat(connectionFailureCallbackInvoked).extracting(CountDownLatch::getCount).isEqualTo(0L);
     }
   }
 
