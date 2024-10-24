42a37dcd0cdf8656ea4fb7b11bac0726652204b5
Marc Nuri <marc@marcnuri.com>
2024-10-24 14:29:23 +0200

    feat(openapi): add component extensions for interfaces and implementations
    Istio/Protobuf
    
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- kubernetes-model-generator/openapi/generator/pkg/openapi/openapi-gen.go
+++ kubernetes-model-generator/openapi/generator/pkg/openapi/openapi-gen.go
@@ -24,7 +24,6 @@ import (
 	"k8s.io/gengo/v2/types"
 	openapiargs "k8s.io/kube-openapi/cmd/openapi-gen/args"
 	"k8s.io/kube-openapi/pkg/generators"
-	"reflect"
 	"strings"
 	"unicode"
 )
@@ -42,6 +41,8 @@ func (g *GoGenerator) Generate() error {
 		processMapKeyTypes,
 		processOmitPrivateFields,
 		processPatchComments,
+		processProtobufOneof,
+		processProtobufTags,
 		processSwaggerIgnore,
 	}
 	return gengo.Execute(
@@ -123,7 +124,7 @@ func (g *GoGenerator) KubernetesFilterFunc(c *generator.Context, t *types.Type) 
 		return false
 	}
 	// Skip types that are not exported
-	if t.Name.Name == "" || unicode.IsUpper(rune(t.Name.Name[0])) == false {
+	if t.Name.Name == "" || !unicode.IsUpper(rune(t.Name.Name[0])) {
 		return false
 	}
 	// There is a conflict between this codegen and codecgen, we should avoid types generated for codecgen
@@ -154,56 +155,3 @@ func (g *GoGenerator) KubernetesFilterFunc(c *generator.Context, t *types.Type) 
 	return false
 }
 
-////////////////////////////////////////////
-// Processors used to fix specific issues //
-////////////////////////////////////////////
-
-// processMapKeyTypes function to process the map key types and replace them by string in case they are not
-// https://github.com/kubernetes/kube-openapi/blob/67ed5848f094e4cd74f5bdc458cd98f12767c538/pkg/generators/openapi.go#L1062-L1065
-// Example errors:
-// - failed to generate map property in k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1.HistogramCheckpoint: BucketWeights: map with non-string keys are not supported by OpenAPI in map[int]uint32
-// - failed to generate map property in istio.io/api/security/v1beta1.PeerAuthentication: PortLevelMtls: map with non-string keys are not supported by OpenAPI in map[uint32]*istio.io/api/security/v1beta1.PeerAuthentication_MutualTLS
-func processMapKeyTypes(context *generator.Context, _ *types.Package, t *types.Type, m *types.Member, memberIndex int) {
-	if m.Type.Kind == types.Map && m.Type.Key != nil && m.Type.Key.Name.Name != "string" {
-		t.Members[memberIndex].Type.Key = context.Universe.Type(types.Name{Name: "string"})
-	}
-}
-
-// processOmitPrivateFields
-// Ignore private fields by adding the json:"-" tag
-func processOmitPrivateFields(_ *generator.Context, _ *types.Package, t *types.Type, m *types.Member, memberIndex int) {
-	// Skip types that are not exported
-	if !unicode.IsUpper(rune(m.Name[0])) {
-		t.Members[memberIndex].Tags = "json:\"-\""
-	}
-}
-
-var patchTags = []string{"patchStrategy", "patchMergeKey"}
-
-// processPatchComments function to process the patchStrategy and patchMergeKey comment tags and add them to the field tags if necessary
-// See https://github.com/fabric8io/kubernetes-client/issues/6426#issuecomment-2431653451
-func processPatchComments(_ *generator.Context, _ *types.Package, t *types.Type, m *types.Member, memberIndex int) {
-	for _, patchTag := range patchTags {
-		tag := reflect.StructTag(m.Tags).Get(patchTag)
-		if tag != "" {
-			continue // Value already set, there is no problem
-		}
-		tags, ok := gengo.ExtractCommentTags("+", m.CommentLines)[patchTag]
-		if ok {
-			t.Members[memberIndex].Tags = t.Members[memberIndex].Tags + " " + patchTag + ":\"" + tags[0] + "\""
-		}
-	}
-}
-
-// processSwaggerIgnore function to process the swaggerignore tag and add json:omitted so that kube-openapi ignores the field.
-func processSwaggerIgnore(_ *generator.Context, _ *types.Package, t *types.Type, m *types.Member, memberIndex int) {
-	swaggerIgnore := reflect.StructTag(m.Tags).Get("swaggerignore")
-	if swaggerIgnore != "" {
-		jsonTag := reflect.StructTag(m.Tags).Get("json")
-		if jsonTag == "" {
-			t.Members[memberIndex].Tags = m.Tags + " json:\"-\""
-		} else {
-			t.Members[memberIndex].Tags = strings.Replace(m.Tags, jsonTag, ",omitted", 1)
-		}
-	}
-}
