d40514ae50bb061e957fd0bbd049847ccedfab02
vaidikcode <vaidikbhardwaj00@gmail.com>
2024-10-28 15:25:07 +0530

    spotless format fix

--- kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/Config.java
+++ kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/Config.java
@@ -51,7 +51,6 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
