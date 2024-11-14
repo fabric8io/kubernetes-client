cfeee83d13d8c8d5dad6d98cb5af19f3dc409f2c
Marc Nuri <marc@marcnuri.com>
2024-11-14 10:10:00 +0100

    fix: remove unneeded json-schema.org JSON files
    - Removed kube-schema.json files
    - Removed validation-schema.json files
    
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- kubernetes-model-generator/kubernetes-model-coordination/src/main/java/io/fabric8/kubernetes/api/model/coordination/v1/LeaseSpec.java
+++ kubernetes-model-generator/kubernetes-model-coordination/src/main/java/io/fabric8/kubernetes/api/model/coordination/v1/LeaseSpec.java
@@ -15,17 +15,32 @@
  */
 package io.fabric8.kubernetes.api.model.coordination.v1;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.fabric8.kubernetes.api.builder.Editable;
+import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.ContainerPort;
+import io.fabric8.kubernetes.api.model.EnvVar;
+import io.fabric8.kubernetes.api.model.IntOrString;
 import io.fabric8.kubernetes.api.model.KubernetesResource;
-import io.fabric8.kubernetes.model.annotation.Generated;
+import io.fabric8.kubernetes.api.model.LabelSelector;
+import io.fabric8.kubernetes.api.model.LocalObjectReference;
+import io.fabric8.kubernetes.api.model.ObjectMeta;
+import io.fabric8.kubernetes.api.model.ObjectReference;
+import io.fabric8.kubernetes.api.model.PersistentVolumeClaim;
+import io.fabric8.kubernetes.api.model.PodTemplateSpec;
+import io.fabric8.kubernetes.api.model.ResourceRequirements;
+import io.fabric8.kubernetes.api.model.Volume;
+import io.fabric8.kubernetes.api.model.VolumeMount;
 import io.sundr.builder.annotations.Buildable;
+import io.sundr.builder.annotations.BuildableReference;
 import lombok.EqualsAndHashCode;
-import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
@@ -33,8 +48,8 @@ import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Generated("org.jsonschema2pojo")
 @JsonPropertyOrder({
     "apiVersion",
     "kind",
@@ -45,16 +60,28 @@ import java.util.Map;
     "leaseTransitions",
     "renewTime"
 })
-@JsonDeserialize(using = com.fasterxml.jackson.databind.JsonDeserializer.None.class)
 @ToString
 @EqualsAndHashCode
-@Setter
 @Accessors(prefix = {
     "_",
     ""
 })
-@Buildable(editableEnabled = false, validationEnabled = false, generateBuilderPackage = false, lazyCollectionInitEnabled = false, builderPackage = "io.fabric8.kubernetes.api.builder")
-public class LeaseSpec implements KubernetesResource {
+@Buildable(editableEnabled = false, validationEnabled = false, generateBuilderPackage = false, lazyCollectionInitEnabled = false, builderPackage = "io.fabric8.kubernetes.api.builder", refs = {
+    @BuildableReference(ObjectMeta.class),
+    @BuildableReference(LabelSelector.class),
+    @BuildableReference(Container.class),
+    @BuildableReference(PodTemplateSpec.class),
+    @BuildableReference(ResourceRequirements.class),
+    @BuildableReference(IntOrString.class),
+    @BuildableReference(ObjectReference.class),
+    @BuildableReference(LocalObjectReference.class),
+    @BuildableReference(PersistentVolumeClaim.class),
+    @BuildableReference(EnvVar.class),
+    @BuildableReference(ContainerPort.class),
+    @BuildableReference(Volume.class),
+    @BuildableReference(VolumeMount.class)
+})
+public class LeaseSpec implements Editable<LeaseSpecBuilder>, KubernetesResource {
 
   @JsonProperty("acquireTime")
   @JsonFormat(timezone = "UTC", pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'")
@@ -83,48 +110,74 @@ public class LeaseSpec implements KubernetesResource {
     this.renewTime = renewTime;
   }
 
+  @JsonProperty("acquireTime")
   public ZonedDateTime getAcquireTime() {
     return acquireTime;
   }
 
+  @JsonProperty("acquireTime")
   public void setAcquireTime(ZonedDateTime acquireTime) {
     this.acquireTime = acquireTime;
   }
 
+  @JsonProperty("holderIdentity")
   public String getHolderIdentity() {
     return holderIdentity;
   }
 
+  @JsonProperty("holderIdentity")
   public void setHolderIdentity(String holderIdentity) {
     this.holderIdentity = holderIdentity;
   }
 
+  @JsonProperty("leaseDurationSeconds")
   public Integer getLeaseDurationSeconds() {
     return leaseDurationSeconds;
   }
 
+  @JsonProperty("leaseDurationSeconds")
   public void setLeaseDurationSeconds(Integer leaseDurationSeconds) {
     this.leaseDurationSeconds = leaseDurationSeconds;
   }
 
+  @JsonProperty("leaseTransitions")
   public Integer getLeaseTransitions() {
     return leaseTransitions;
   }
 
+  @JsonProperty("leaseTransitions")
   public void setLeaseTransitions(Integer leaseTransitions) {
     this.leaseTransitions = leaseTransitions;
   }
 
+  @JsonProperty("renewTime")
   public ZonedDateTime getRenewTime() {
     return renewTime;
   }
 
+  @JsonProperty("renewTime")
   public void setRenewTime(ZonedDateTime renewTime) {
     this.renewTime = renewTime;
   }
 
+  @JsonIgnore
+  public LeaseSpecBuilder edit() {
+    return new LeaseSpecBuilder(this);
+  }
+
+  @JsonIgnore
+  public LeaseSpecBuilder toBuilder() {
+    return edit();
+  }
+
+  @JsonAnyGetter
   public Map<String, Object> getAdditionalProperties() {
-    return additionalProperties;
+    return this.additionalProperties;
+  }
+
+  @JsonAnySetter
+  public void setAdditionalProperty(String name, Object value) {
+    this.additionalProperties.put(name, value);
   }
 
   public void setAdditionalProperties(Map<String, Object> additionalProperties) {
