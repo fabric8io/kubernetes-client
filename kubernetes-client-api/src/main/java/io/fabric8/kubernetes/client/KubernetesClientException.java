53434454e006cfef92e7cf41d0dac630113ca4dd
Ashutosh Sharan <asharan2@buffalo.edu>
2024-11-20 12:17:46 -0500

    fix: removed deprecated methods from KubernetesClientException class (6633)
    Remove deprecated methods from KubernetesClientException class #6629
    
    Remove deprecated constructors and methods and ran the tests to ensure nothing is failing
    ---
    Update KubernetesClientException.java
    
    Ran the mvn spotless:apply
    ---
    Update KubernetesClientException.java
    
    Removed comments

--- kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/KubernetesClientException.java
+++ kubernetes-client-api/src/main/java/io/fabric8/kubernetes/client/KubernetesClientException.java
@@ -63,26 +63,6 @@ public class KubernetesClientException extends RuntimeException {
     this.requestMetadata = requestMetadata == null ? RequestMetadata.EMPTY : requestMetadata;
   }
 
-  /**
-   * @deprecated use {@link #KubernetesClientException(String, Throwable, int, Status, HttpRequest)} instead
-   */
-  @Deprecated
-  public KubernetesClientException(String message, int code, Status status, String group, String version, String resourcePlural,
-      String namespace) {
-    this(message, null, code, status,
-        RequestMetadata.builder().group(group).version(version).plural(resourcePlural).namespace(namespace).build());
-  }
-
-  /**
-   * @deprecated use {@link #KubernetesClientException(String, Throwable, int, Status, HttpRequest)} instead
-   */
-  @Deprecated
-  public KubernetesClientException(String message, Throwable t, String group, String version, String resourcePlural,
-      String namespace) {
-    this(message, t, -1, null,
-        RequestMetadata.builder().group(group).version(version).plural(resourcePlural).namespace(namespace).build());
-  }
-
   public Status getStatus() {
     return status;
   }
@@ -157,19 +137,6 @@ public class KubernetesClientException extends RuntimeException {
     throw new KubernetesClientException(sb.toString(), cause, -1, null, RequestMetadata.builder()
         .group(spec.getGroup()).version(spec.getVersion()).plural(spec.getPlural())
         .namespace(spec.getNamespace()).name(spec.getName()).build());
-  }
-
-  /**
-   * @deprecated Use {@link #launderThrowable(OperationInfo, Throwable)} instead
-   */
-  @Deprecated
-  public static RuntimeException launderThrowable(OperationInfo spec, Status status, Throwable cause) {
-    StringBuilder sb = new StringBuilder();
-    sb.append(describeOperation(spec)).append(" failed.");
-    if (status != null && Utils.isNotNullOrEmpty(status.getMessage())) {
-      sb.append("Reason: ").append(status.getMessage());
-    }
-    return launderThrowable(sb.toString(), cause);
   }
 
   private static String describeOperation(OperationInfo operation) {
